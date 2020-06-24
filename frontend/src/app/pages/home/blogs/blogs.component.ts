8cfb7d8cf0e753d82874c8de553ff7c3e3adaf5c
Landon Mote <figments@outlook.com>
2020-06-24 16:02:36 -0700

    Linked create blog form to page

--- frontend/src/app/pages/home/blogs/blogs.component.ts
+++ frontend/src/app/pages/home/blogs/blogs.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Toppy, ToppyControl } from 'toppy';
+import { Toppy, ToppyControl, GlobalPosition, InsidePlacement } from 'toppy';
 
 import { BlogsService } from 'src/app/services/content';
 import { AuthService } from 'src/app/services/auth';
+import { CreateBlogComponent } from 'src/app/components/modals';
 import { User } from 'src/app/models/users';
 import { Blog } from 'src/app/models/blogs';
 
@@ -15,17 +16,65 @@ import { Blog } from 'src/app/models/blogs';
 export class BlogsComponent implements OnInit {
   currentUser: User;
   blogs: Blog[];
+  loading = false;
+
   searchBlogs = new FormGroup({
     query: new FormControl('', Validators.required),
   });
+
   filterOptions: ToppyControl;
-  createEditBlog: ToppyControl;
+  createBlog: ToppyControl;
   previewBlog: ToppyControl;
 
-  constructor(private blogsService: BlogsService, private authService: AuthService, private toppy: Toppy) { }
-
-  ngOnInit(): void {
+  constructor(private blogsService: BlogsService, private authService: AuthService, private toppy: Toppy) {
+    this.authService.currUser.subscribe(x => this.currentUser = x);
+    this.fetchData();
   }
 
-  
+  ngOnInit(): void {
+    const position = new GlobalPosition({
+      placement: InsidePlacement.CENTER,
+      width: 'auto',
+      height: 'auto',
+    });
+
+    this.createBlog = this.toppy
+      .position(position)
+      .config({backdrop: true, closeOnEsc: true})
+      .content(CreateBlogComponent)
+      .create();
+
+    this.createBlog.listen('t_close').subscribe(() => {
+      this.fetchData();
+    });
+  }
+
+  private fetchData() {
+    this.loading = true;
+    this.blogsService.fetchUserBlogs().subscribe(blogs => {
+      this.blogs = blogs;
+      this.loading = false;
+    });
+  }
+
+  isBlogsEmpty() {
+    if (this.blogs) {
+      if (this.blogs.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  openNewBlogForm() {
+    this.createBlog.open();
+  }
+
+  askDelete(blogId: string) {
+    if (confirm('Are you sure you want to delete this blog? This action is irreversible.')) {
+      // TODO: Implement blog deletion both server side and client side.
+      return;
+    }
+  }
 }
