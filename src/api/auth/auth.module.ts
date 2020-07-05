7f089deff4eca646e24551b7b8205da400281ba6
Landon Mote <figments@outlook.com>
2020-07-05 00:15:08 -0700

    Temporarily changed JWT expiration to one month from creation

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { LocalStrategy } from './local.strategy';
     JwtModule.registerAsync({
       useFactory: () => ({
         secret: process.env.JWT_SECRET,
-        signOptions: { expiresIn: '3600s' },
+        signOptions: { expiresIn: '2628000s' },
       }),
     }),
   ],
