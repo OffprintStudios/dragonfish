5c46da95d0f0bd5dd0c191f7cf1d4fa4994cb1a3
Alyx Mote <figments@outlook.com>
2020-07-21 16:27:45 -0700

    Frontend services prepped for portfolios

--- frontend/src/app/pages/portfolio/portfolio.component.ts
+++ frontend/src/app/pages/portfolio/portfolio.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { User } from 'src/app/models/users';
+import { AuthService } from 'src/app/services/auth';
+import { PortfolioService } from 'src/app/services/content';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,12 +11,28 @@ import { User } from 'src/app/models/users';
   styleUrls: ['./portfolio.component.less']
 })
 export class PortfolioComponent implements OnInit {
-  portUser: User;
+  portUser: User; // The user whose portfolio this is
+  portUserId: string; // Their ID, fetched from the route parameters
 
+  currentUser: User;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute, private portService: PortfolioService) {
+    this.authService.currUser.subscribe(x => {
+      this.currentUser = x;
+    });
+    this.fetchData();
+  }
 
   ngOnInit(): void {
+  }
+
+  private fetchData() {
+    this.route.paramMap.subscribe(params => {
+      this.portUserId = params.get('id');
+    });
+    this.portService.getUserInfo(this.portUserId).subscribe(x => {
+      this.portUser = x;
+    })
   }
 
 }
