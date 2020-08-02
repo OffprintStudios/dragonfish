35f92eff57d69ce074eee6365208c4c44724b9ab
Alyx Mote <figments@outlook.com>
2020-08-01 19:03:12 -0700

    Updating the production build script

--- build_prod.sh
+++ build_prod.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Installing dependencies for server and building
+yarn add @types/mongoose-paginate-v2 --dev # This one keeps failing to install
 yarn install
 yarn build
 
 # Installing dependencies for frontend and building
 cd frontend
+yarn add @angular-devkit/build-angular --dev
 yarn install
 yarn build --prod
 
