093c13a4665293ad7451ddaa8b05d799c31795ca
Alyx Mote <figments@outlook.com>
2020-08-05 12:29:14 -0700

    Removed references to the RolesGuard

--- src/api/contrib/contrib.controller.ts
+++ src/api/contrib/contrib.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, UseGuards, Request, Get } from '@nestjs/common';
 
 import { ContribService } from './contrib.service';
-import { Roles } from 'src/db/users/models';
-import { RolesGuard } from 'src/guards';
+import { AuthGuard } from 'src/guards';
 
 @Controller()
 export class ContribController {
     constructor(private readonly contribService: ContribService) {}
 
-    @UseGuards(RolesGuard([Roles.User]))
+    @UseGuards(AuthGuard)
     @Get('roles')
     async getRoles(@Request() req: any) {
         return await this.contribService.checkRoles(req.user);
