8770388dbeb2fc6a352eab6765a1efb89af72049
Alyx Mote <figments@outlook.com>
2020-08-06 22:53:10 -0700

    More progress on stuff

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { User } from 'src/app/models/users';
+import { ApprovalQueue, Decision } from 'src/app/models/admin';
 import { AuthService } from 'src/app/services/auth';
+import { QueueService } from 'src/app/services/admin';
 
 @Component({
   selector: 'app-contrib',
@@ -8,13 +11,45 @@ import { AuthService } from 'src/app/services/auth';
   styleUrls: ['./dashboard.component.less']
 })
 export class DashboardComponent implements OnInit {
-  currentUser: User;
+  currentUser: User; // the currently logged-in user
 
-  constructor(private authService: AuthService) {
+  loading = false; // loading check
+  queue: ApprovalQueue[]; // the approval queue
+  queueForMod: ApprovalQueue[]; // the 
+
+  forMe = false; // for switching views
+
+  constructor(private authService: AuthService, private queueService: QueueService) {
     this.authService.currUser.subscribe(x => { this.currentUser = x; });
+    this.fetchData();
   }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Fetches entries based on which setting it is.
+   * 
+   * @param which Which entries to fetch
+   */
+  private fetchData() {
+    this.queueService.getQueue().subscribe(entries => {
+      this.queue = entries.reverse();
+    });
+
+    this.queueService.getQueueForMod().subscribe(entries => {
+      this.queueForMod = entries.reverse();
+    });
+  }
+
+  /**
+   * Changes the queue view from all to personal.
+   */
+  changeQueueView() {
+    if (this.forMe === true) {
+      this.forMe = false;
+    } else {
+      this.forMe = true;
+    }
+  }
 }
