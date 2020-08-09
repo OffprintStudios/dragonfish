2324bf99d9512c945b7ac6c8a56b4f012ed78bf3
pingzing <mcalistern@gmail.com>
2020-08-09 06:24:25 +0300

    Actually handle errors on the browse page

--- frontend/src/app/pages/browse/browse.component.ts
+++ frontend/src/app/pages/browse/browse.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import * as models from 'src/app/models/works';
 import { BrowseService } from 'src/app/services/content/browse.service';
+import { AlertsService } from 'src/app/modules/alerts';
+import { HttpError } from 'src/app/models/site';
+import { HttpErrorResponse } from '@angular/common/http';
 
 type LoadingState = 'notstarted' | 'loading' | 'success' | 'failure';
 
@@ -14,7 +17,7 @@ export class BrowseComponent implements OnInit {
   loadingState: LoadingState = 'notstarted';
   works: models.Work[] = [];
 
-  constructor(private browseService: BrowseService) { 
+  constructor(private browseService: BrowseService, private alertService: AlertsService) { 
     this.fetchData();
   }
 
@@ -25,12 +28,15 @@ export class BrowseComponent implements OnInit {
     this.loadingState = 'loading';    
     this.browseService.fetchAllPublishedWorks().subscribe(allWorks => {
       if (!allWorks || allWorks.length === 0) {
-        this.loadingState = "failure";
+        this.loadingState = 'failure';
       } else {
-        this.loadingState = "success";
+        this.loadingState = 'success';
       }
 
       this.works = allWorks;
+    }, (err: HttpErrorResponse) => {
+      this.loadingState = 'failure';
+      this.alertService.error(`Failed to retrieve stories. ${err.message}`);
     });
   }
 }
