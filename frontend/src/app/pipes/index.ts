5a83e410b22c20596a21f6981b2de9805b886b74
Alyx Mote <figments@outlook.com>
2020-08-09 21:15:05 -0700

    Updated browse page with new look

--- frontend/src/app/pipes/index.ts
+++ frontend/src/app/pipes/index.ts
@@ -3,3 +3,4 @@ export { PluralizePipe } from './pluralize.pipe';
 export { SeparateEntitiesPipe } from './separate-entities.pipe';
 export { FixCategoriesPipe } from './fix-categories.pipe';
 export { StringifyMetaPipe } from './stringify-meta.pipe';
+export { ToLocaleStringPipe } from './to-locale-string.pipe';
