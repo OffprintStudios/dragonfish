892c3abe51a97d06af0920a5f51ab242dce41a4c
Alyx Mote <figments@outlook.com>
2020-08-13 10:10:35 -0700

    user dropdown now on the avatar

--- frontend/src/app/pages/dashboard/docs/docs.component.ts
+++ frontend/src/app/pages/dashboard/docs/docs.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
+import * as models from 'src/app/models/admin/docs';
 import { User } from 'src/app/models/users';
+import { DocsService } from 'src/app/services/admin';
+import { AuthService } from 'src/app/services/auth';
 
 @Component({
   selector: 'app-docs',
@@ -8,10 +12,20 @@ import { User } from 'src/app/models/users';
 })
 export class DocsComponent implements OnInit {
   currentUser: User;
+  docs: models.Doc[];
 
-  constructor() { }
+  constructor(private docsService: DocsService, private authService: AuthService) {
+    this.authService.currUser.subscribe(x => { this.currentUser = x; });
+    this.fetchData();
+  }
 
   ngOnInit(): void {
   }
 
+  private fetchData() {
+    this.docsService.fetchForDashboard().subscribe(docs => {
+      this.docs = docs;
+      console.log(docs);
+    });
+  }
 }
