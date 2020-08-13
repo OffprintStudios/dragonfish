7a68f65b10573f1b039fc422cbb5f55e26cb8b2a
Alyx Mote <figments@outlook.com>
2020-08-13 12:07:53 -0700

    Added create doc and edit doc pages

--- frontend/src/app/pages/dashboard/docs/docs.component.ts
+++ frontend/src/app/pages/dashboard/docs/docs.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as lodash from 'lodash';
 
 import * as models from 'src/app/models/admin/docs';
-import { User } from 'src/app/models/users';
+import { User, Roles } from 'src/app/models/users';
 import { DocsService } from 'src/app/services/admin';
 import { AuthService } from 'src/app/services/auth';
+import { AlertsService } from 'src/app/modules/alerts';
 
 @Component({
   selector: 'app-docs',
@@ -14,7 +17,8 @@ export class DocsComponent implements OnInit {
   currentUser: User;
   docs: models.Doc[];
 
-  constructor(private docsService: DocsService, private authService: AuthService) {
+  constructor(private docsService: DocsService, private authService: AuthService,
+    private router: Router, private alertsService: AlertsService, public route: ActivatedRoute) {
     this.authService.currUser.subscribe(x => { this.currentUser = x; });
     this.fetchData();
   }
@@ -22,10 +26,28 @@ export class DocsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * Fetches all docs from the backend.
+   */
   private fetchData() {
     this.docsService.fetchForDashboard().subscribe(docs => {
       this.docs = docs;
       console.log(docs);
     });
+  }
+
+  switchToCreateDoc() {
+    if (this.currentUser) {
+      const allowedRoles: Roles[] = [Roles.Admin];
+      const isAllowed = lodash.intersection(allowedRoles, this.currentUser.roles);
+      if (isAllowed.length > 0) {
+        this.router.navigate(['/dashboard/docs/create-doc']);
+        return;
+      } else {
+        this.alertsService.error(`You must be an admin to create new site docs.`);
+      }
+    } else {
+      return;
+    }
   }
 }
