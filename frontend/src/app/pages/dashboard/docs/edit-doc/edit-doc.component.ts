b256bc92e4a8ed0cf4eb0dd3eb99c6dd193f2577
Alyx Mote <figments@outlook.com>
2020-08-13 13:04:27 -0700

    update package.json build script

--- frontend/src/app/pages/dashboard/docs/edit-doc/edit-doc.component.ts
+++ frontend/src/app/pages/dashboard/docs/edit-doc/edit-doc.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { User, Roles } from 'shared-models';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth';
+import { DocsService } from 'src/app/services/admin';
+import { AlertsService } from 'src/app/modules/alerts';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-doc',
@@ -6,8 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-doc.component.less']
 })
 export class EditDocComponent implements OnInit {
+  currentUser: User;
+  rolesList = Roles;
+  loading = false;
 
-  constructor() { }
+  editorFormats = [
+    'header', 'bold', 'italic', 'underline', 'strike',
+    'divider', 'link', 'blockquote', 'code', 'image',
+    'align', 'center', 'right', 'justify',
+    'list', 'bullet', 'ordered'
+  ];
+
+  editDocForm = new FormGroup({
+    id: new FormControl('', [Validators.required]),
+    docName: new FormControl('', [Validators.required]),
+    docDesc: new FormControl('', [Validators.required]),
+    docBody: new FormControl('', [Validators.required]),
+    approvedRoles: new FormControl([], [Validators.required]),
+  });
+
+  constructor(private authService: AuthService, private docsService: DocsService,
+    private cdr: ChangeDetectorRef, private alertsService: AlertsService, private router: Router) {
+      this.authService.currUser.subscribe(x => { this.currentUser = x; });
+    }
 
   ngOnInit(): void {
   }
