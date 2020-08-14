3f0055e06feaa566c75b8416755a699029ec517b
Alyx Mote <figments@outlook.com>
2020-08-14 13:02:59 -0700

    more work on displaying collections

--- frontend/src/app/components/modals/collections/create-collection/create-collection.component.ts
+++ frontend/src/app/components/modals/collections/create-collection/create-collection.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CollectionsService } from 'src/app/services/content';
+import { AlertsService } from 'src/app/modules/alerts';
+import { CreateCollection } from 'shared-models';
 
 @Component({
   selector: 'app-create-collection',
@@ -6,10 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-collection.component.less']
 })
 export class CreateCollectionComponent implements OnInit {
+  close: any;
 
-  constructor() { }
+  createCollectionForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(32)]),
+    desc: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+    public: new FormControl(false)
+  });
 
-  ngOnInit(): void {
+  constructor(private collsService: CollectionsService, private alertsService: AlertsService) { }
+
+  ngOnInit(): void {}
+
+  /**
+   * Create collection form getter.
+   */
+  get fields() { return this.createCollectionForm.controls; }
+
+  /**
+   * Submits the new collection form for processing.
+   */
+  submitCollection() {
+    if (this.fields.name.invalid) {
+      this.alertsService.warn(`Collections must have a name.`);
+      return;
+    }
+
+    if (this.fields.desc.invalid) {
+      this.alertsService.warn(`Collections must have a description.`);
+      return;
+    }
+
+    const newCollection: CreateCollection = {
+      name: this.fields.name.value,
+      desc: this.fields.desc.value,
+      public: this.fields.public.value
+    };
+
+    this.collsService.createCollection(newCollection).subscribe(() => {
+      this.close();
+    });
   }
 
+  /**
+   * Ask to cancel form submission.
+   */
+  askCancel() {
+    if (this.createCollectionForm.dirty || this.createCollectionForm.touched) {
+      if (confirm('Are you sure? Any unsaved changes will be lost.')) {
+        this.close();
+      } else {
+        return;
+      }
+    } else {
+      this.close();
+      return;
+    }
+  }
 }
