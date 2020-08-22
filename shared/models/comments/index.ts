3bfb08fc6abc6513faa1148caabcb6fa2668a851
Alyx Mote <figments@outlook.com>
2020-08-22 00:42:44 -0700

    Added create comment methods

--- shared/models/comments/index.ts
+++ shared/models/comments/index.ts
@@ -1,1 +1,2 @@
 export { Comment, BlogComment, WorkComment } from './comment.model';
+export { CreateComment } from './create-comment.model';
--- src/db/comments/comments.service.ts
+++ src/db/comments/comments.service.ts
@@ -3,11 +3,44 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import * as documents from './models';
+import * as models from 'shared/models/comments';
 
 @Injectable()
 export class CommentsService {
-    constructor(@InjectModel('BlogComment') blogCommentModel: Model<documents.BlogCommentDocument>,
-        @InjectModel('WorkComment') workCommentModel: Model<documents.WorkCommentDocument>) {}
+    constructor(@InjectModel('BlogComment') private readonly blogCommentModel: Model<documents.BlogCommentDocument>,
+        @InjectModel('WorkComment') private readonly workCommentModel: Model<documents.WorkCommentDocument>) {}
 
-    
+    /**
+     * Creates a new comment that belongs to a blog.
+     * 
+     * @param user A user's JWT payload
+     * @param blogId The blog the comment belongs to
+     * @param commentInfo The comment's info
+     */
+    async createBlogComment(user: any, blogId: string, commentInfo: models.CreateComment) {
+        const newComment = new this.blogCommentModel({
+            user: user.sub,
+            blogId: blogId,
+            body: commentInfo.body
+        });
+
+        return await newComment.save();
+    }
+
+    /**
+     * Creates a new comment that belongs to a work.
+     * 
+     * @param user A user's JWT payload
+     * @param workId The work the comment belongs to
+     * @param commentInfo The comment's info
+     */
+    async createWorkComment(user: any, workId: string, commentInfo: models.CreateComment) {
+        const newComment = new this.workCommentModel({
+            user: user.sub,
+            workId: workId,
+            body: commentInfo.body
+        });
+
+        return await newComment.save();
+    }
 }
