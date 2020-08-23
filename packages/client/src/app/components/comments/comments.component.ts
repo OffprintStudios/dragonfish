b605b218cc0fda8e9cf7e1f1df7f61d664d2e9b3
Alyx Mote <figments@outlook.com>
2020-08-23 14:10:42 -0700

    added fetch data to comments component

--- packages/client/src/app/components/comments/comments.component.ts
+++ packages/client/src/app/components/comments/comments.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Toppy, ToppyControl } from 'toppy';
+
 import { FrontendUser } from '@pulp-fiction/models/users';
+import { BlogComment, WorkComment } from '@pulp-fiction/models/comments';
 import { AuthService } from '../../services/auth';
+import { CommentsService } from '../../services/content';
 
 @Component({
   selector: 'comments',
@@ -11,12 +15,32 @@ export class CommentsComponent implements OnInit {
   @Input() itemId: string;
   @Input() itemKind: string;
   @Input() pageNum: number;
+  @Input() banlist?: any;
 
   currentUser: FrontendUser;
+  loading = false;
+  comments: BlogComment[] | WorkComment[];
+  commentForm: ToppyControl;
 
-  constructor(private authService: AuthService) { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private toppy: Toppy, private commentsService: CommentsService) { 
+    this.authService.currUser.subscribe(x => { this.currentUser = x; });
+    this.fetchData();
   }
 
+  ngOnInit(): void {}
+
+  fetchData(/*pageNum: number*/) {
+    this.loading = true;
+    if (this.itemKind === 'Blog') {
+      this.commentsService.getBlogComments(this.itemId).subscribe(comments => {
+        this.comments = comments;
+        this.loading = false;
+      });
+    } else if (this.itemKind === 'Work') {
+      this.commentsService.getWorkComments(this.itemId).subscribe(comments => {
+        this.comments = comments;
+        this.loading = false;
+      });
+    }
+  }
 }
