644983d8fe781ec8e3c1f4df0ee2f1a356742b10
Alyx Mote <figments@outlook.com>
2020-08-31 21:36:44 -0700

    rearranging buttons

--- packages/client/src/app/components/froala-editor/froala-editor.component.ts
+++ packages/client/src/app/components/froala-editor/froala-editor.component.ts
@@ -43,6 +43,7 @@ export class FroalaEditorComponent implements ControlValueAccessor, OnInit {
     imageMove: false, // don't allow dragging of imgaes
     imageUpload: false,
     imageInsertButtons: ['imageBack', '|', 'imageByURL', 'imageManager'], // Default buttons include an "insert directly" button, which we don't allow
+    videoInsertButtons: ['videoBack', '|', 'videoByUrl'],
     toolbarButtons: {
       moreText: {
         buttons: ['paragraphFormat', 'bold', 'italic', 'underline', 'strikeThrough', 'fontSize'],
@@ -50,19 +51,19 @@ export class FroalaEditorComponent implements ControlValueAccessor, OnInit {
         buttonsVisible: 6
       },
       moreParagraph: {
-        buttons: ['alignLeft', 'alignCenter', 'alignRight', 'alignJustify', 'formatUL', 'formatOL'],
+        buttons: ['alignLeft', 'alignCenter', 'alignRight', 'alignJustify', 'formatUL', 'formatOL', 'quote', 'outdent', 'indent'],
         align: 'left',
-        buttonsVisible: 3
+        buttonsVisible: 4
       },
       moreRich: {
-        buttons: ['insertHR', 'insertLink', 'insertImage', 'quote', 'clearFormatting'],
+        buttons: ['clearFormatting', 'insertHR', 'insertLink', 'insertImage', 'insertVideo', 'emoticons'],
         align: 'left',
         buttonsVisible: 3,
       },
       moreMisc: {
-        buttons: ['undo', 'redo', 'fullscreen', 'html'],
+        buttons: ['undo', 'redo', 'fullscreen', 'spellChecker'],
         align: 'right',
-        buttonsVisible: 2
+        buttonsVisible: 4
       }
     }
   }
