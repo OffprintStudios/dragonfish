168da1b63e7b57ce4522601bd15e2fddae63d014
pingzing <mcalistern@gmail.com>
2020-09-03 23:17:19 +0300

    Cache tarred node, fix pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "pulpd",
         script: "/opt/pulpd/site/main.js",
-        cwd: "opt/pulpd/site/",
+        cwd: "/opt/pulpd/site/",
         env_production: {
             "NODE_ENV": "production"
         },
