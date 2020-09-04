4372f840b5b0e7bc0aa378b3dedf85d3002b7445
Alyx Mote <figments@outlook.com>
2020-09-04 09:04:10 -0700

    please work goddammit

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-console.log(`Path to word counter native: ${path.resolve(__dirname, '..', '..', 'native', 'word_counter', 'src')}\n`);
+console.log(`Path to word counter native: ${path.resolve(__dirname, '..', '..', 'native', 'word_counter', 'src')}`);
 console.log(`Path to word counter library: ${path.resolve(__dirname, '..', '..', 'packages', 'word_counter', 'src', 'lib')}\n\n`);
-console.log(`Path to HTML sanitizer native: ${path.resolve(__dirname, '..', '..', 'native', 'html_sanitizer', 'src')}\n`);
+console.log(`Path to HTML sanitizer native: ${path.resolve(__dirname, '..', '..', 'native', 'html_sanitizer', 'src')}`);
 console.log(`Path to HTML sanitizer library: ${path.resolve(__dirname, '..', '..', 'packages', 'html_sanitizer', 'src', 'lib')}\n\n`);
 
 module.exports = (config, context) => {
