7ee2020433770c0b7841f02c818fd738e8488e27
Alyx Mote <figments@outlook.com>
2020-09-03 18:15:40 -0700

    new build script

--- build-prod.sh
+++ build-prod.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
 # Installing dependencies for workspace
+printf "Acquiring all dependences...\n"
+curl https://sh.rustup.rs -sSf |  bash -s -- -y
+echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
 yarn install
+printf "\n"
 
 # Building everything
+printf "Building the server and client...\n"
 nx build server --prod && nx build client --prod
+printf "\n"
 
 # Copying .env file to server
+printf "Copying .env file to server...\n"
 cp .env ./dist/packages/server/.env
+printf "\n"
+
+# Copying client to server and moving into it
+printf "Copying client files to server folder for static file serving...\n"
+rsync -r ./dist/packages/client/* ./dist/packages/server/static
+cd ./dist/packages/server
+printf "\n"
+
+# Exit message
+printf "Ready for blastoff!\n"
