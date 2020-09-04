e924287deb2005e04376c5faa0367a8ef6b042b7
Alyx Mote <figments@outlook.com>
2020-09-04 08:51:47 -0700

    let's make sure all paths are getting resolved

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require("path");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-console.log(`Exporting this webpack config, for word counter: ${path.resolve(__dirname, '..', '..', 'native', 'word_counter', 'src')}`);
+console.log(`Path to word counter native: ${path.resolve(__dirname, '..', '..', 'native', 'word_counter', 'src')}\n`);
+console.log(`Path to word counter library: ${path.resolve(__dirname, '..', '..', 'packages', 'word_counter', 'src', 'lib')}\n\n`);
+console.log(`Path to HTML sanitizer native: ${path.resolve(__dirname, '..', '..', 'native', 'html_sanitizer', 'src')}\n`);
+console.log(`Path to HTML sanitizer library: ${path.resolve(__dirname, '..', '..', 'packages', 'html_sanitizer', 'src', 'lib')}\n\n`);
+
 module.exports = (config, context) => {
     return {
         ...config,        
