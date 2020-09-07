0257c950a75135e5b60a58e9e48d1fae9458a4c6
Alyx Mote <figments@outlook.com>
2020-09-06 19:45:03 -0700

    added constructor to service

--- packages/server/src/app/db/messages/messages.service.ts
+++ packages/server/src/app/db/messages/messages.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { PaginateModel } from 'mongoose';
+
+import { MessageDocument } from './message.schema';
+import { MessageThreadDocument } from './message-thread.schema';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+    constructor(@InjectModel('Message') private readonly messageModel: PaginateModel<MessageDocument>,
+                @InjectModel('MessageThread') private readonly messageThreadModel: PaginateModel<MessageThreadDocument>) {}
+
+    
+}
