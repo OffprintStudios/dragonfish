6c59c912ef7b8cdf7f12913d619cc5db5ed6ca63
Alyx Mote <figments@outlook.com>
2020-09-07 21:02:19 -0700

    Added controller routes

--- packages/server/src/app/api/content/messages/messages.controller.ts
+++ packages/server/src/app/api/content/messages/messages.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Request, Param, Get, Put, Body } from '@nestjs/common';
+
+import { RolesGuard } from '../../../guards';
+import { Roles } from '@pulp-fiction/models/users';
+import { CreateResponse, CreateInitialMessage } from '@pulp-fiction/models/messages';
+import { MessagesService } from '../../../db/messages/messages.service';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    constructor (private readonly messageService: MessagesService) {}
+
+    @UseGuards(RolesGuard([Roles.User]))
+    @Get('fetch-user-threads/:pageNum')
+    async fetchUserThreads(@Request() req: any, @Param('pageNum') pageNum: number) {
+        return await this.messageService.fetchThreads(req.user, pageNum);
+    }
+
+    @UseGuards(RolesGuard([Roles.User]))
+    @Put('create-new-private-thread')
+    async createNewPrivateThread(@Request() req: any, @Body() initialMessage: CreateInitialMessage) {
+        return await this.messageService.createNewPrivateThread(req.user, initialMessage);
+    }
+
+    @UseGuards(RolesGuard([Roles.User]))
+    @Put('create-response')
+    async createResponse(@Request() req: any, @Body() response: CreateResponse) {
+        return await this.messageService.createResponse(req.user, response);
+    }
+}
