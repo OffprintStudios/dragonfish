181ac51d07c15b11dd4a80a7ac88e584f195edb7
Alyx Mote <figments@outlook.com>
2020-09-10 10:49:06 -0700

    form is built

--- packages/client/src/app/components/portfolio/start-conversation/start-conversation.component.ts
+++ packages/client/src/app/components/portfolio/start-conversation/start-conversation.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { CreateInitialMessage } from '@pulp-fiction/models/messages';
+import { MessagesService } from '../../../services/content';
+import { AlertsService } from '../../../modules/alerts';
 
 @Component({
   selector: 'pulp-fiction-start-conversation',
@@ -6,10 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-conversation.component.less']
 })
 export class StartConversationComponent implements OnInit {
+  loading = false;
 
-  constructor() { }
+  newConversation = new FormGroup({
+    subject: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(36)]),
+    message: new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
+
+  constructor(private messageService: MessagesService, private alertsService: AlertsService,
+    private dialogRef: MatDialogRef<StartConversationComponent>) { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Getter for the new conversation form fields
+   */
+  get fields() { return this.newConversation.controls; }
+
+  
 }
