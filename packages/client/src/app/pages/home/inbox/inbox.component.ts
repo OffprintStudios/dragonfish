0387576bf3cc0b5f30693d54c293cd674c014619
Alyx Mote <figments@outlook.com>
2020-09-14 15:10:41 -0700

    last change to pms

--- packages/client/src/app/pages/home/inbox/inbox.component.ts
+++ packages/client/src/app/pages/home/inbox/inbox.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FrontendUser } from '@pulp-fiction/models/users';
+import { MessageThread } from '@pulp-fiction/models/messages';
+import { PaginateResult } from '@pulp-fiction/models/util';
+import { AuthService } from '../../../services/auth';
+import { MessagesService } from '../../../services/content';
+
 @Component({
   selector: 'app-inbox',
   templateUrl: './inbox.component.html',
   styleUrls: ['./inbox.component.less']
 })
 export class InboxComponent implements OnInit {
+  currentUser: FrontendUser;
+  threads: PaginateResult<MessageThread>;
 
-  constructor() { }
+  pageNum: number = 1;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private messageService: MessagesService) {
+    this.authService.currUser.subscribe(x => {this.currentUser = x;});
+    this.fetchData(this.pageNum);
   }
 
+  ngOnInit(): void {}
+
+  private fetchData(pageNum: number) {
+    this.messageService.fetchUserThreads(pageNum).subscribe(threads => {
+      this.threads = threads;
+      this.pageNum = pageNum;
+    });
+  }
 }
