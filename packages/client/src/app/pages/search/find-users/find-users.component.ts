420e6d4562b49f94b4e6f43de6a94dda0da9f765
Alyx Mote <figments@outlook.com>
2020-09-20 20:11:23 -0700

    Find users page done

--- packages/client/src/app/pages/search/find-users/find-users.component.ts
+++ packages/client/src/app/pages/search/find-users/find-users.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { User } from '@pulp-fiction/models/users';
+import { PaginateResult } from '@pulp-fiction/models/util';
+import { SearchService } from '../../../services/utility';
 
 @Component({
   selector: 'app-find-users',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-users.component.less']
 })
 export class FindUsersComponent implements OnInit {
+  results: PaginateResult<User>;
+  query: string
+  pageNum = 1;
 
-  constructor() { }
+  constructor(private searchService: SearchService, public route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    const queryParams = this.route.snapshot.queryParamMap;    
+    if (queryParams.get('query') !== null && queryParams.get('page') === null) {
+      this.query = queryParams.get('query');
+      this.fetchData(this.query, this.pageNum);
+    } else if (queryParams.get('query') !== null && queryParams.get('page') !== null) {
+      this.query = queryParams.get('query');
+      this.pageNum = +queryParams.get('page');
+      this.fetchData(this.query, this.pageNum);
+    }
   }
 
+  fetchData(query: string, pageNum: number) {
+    this.router.navigate([], {relativeTo: this.route, queryParams: {query: query, page: pageNum}, queryParamsHandling: 'merge'});
+    this.searchService.getUserResults(query, pageNum).subscribe(results => {
+      this.results = results;
+      this.pageNum = pageNum;
+    });
+  }
 }
--- packages/client/src/app/services/utility/search.service.ts
+++ packages/client/src/app/services/utility/search.service.ts
@@ -3,8 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
+import { PaginateResult } from '@pulp-fiction/models/util';
 import { InitialResults } from './models';
 import { AlertsService } from '../../modules/alerts';
+import { Work } from '@pulp-fiction/models/works';
+import { Blog } from '@pulp-fiction/models/blogs';
+import { User } from '@pulp-fiction/models/users';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +24,36 @@ export class SearchService {
         return res.body;
       }), catchError(err => {
         this.alertsService.error(`Something went wrong! Try again in a little bit.`);
+        return throwError(err);
+      }));
+  }
+
+  public getWorkResults(query: string, pageNum: number) {
+    return this.http.get<PaginateResult<Work>>(`${this.url}/get-work-results?query=${query}&pageNum=${pageNum}`, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        this.alertsService.error(`Something went wrong! Try again in a little bit.`);
+        return throwError(err);
+      }));
+  }
+
+  public getBlogResults(query: string, pageNum: number) {
+    return this.http.get<PaginateResult<Blog>>(`${this.url}/get-blog-results?query=${query}&pageNum=${pageNum}`, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        this.alertsService.error(`Something with wrong! Try again in a little bit.`);
+        return throwError(err);
+      }));
+  }
+
+  public getUserResults(query: string, pageNum: number) {
+    return this.http.get<PaginateResult<User>>(`${this.url}/get-user-results?query=${query}&pageNum=${pageNum}`, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        this.alertsService.error(`Something with wrong! Try again in a little bit.`);
         return throwError(err);
       }));
   }
