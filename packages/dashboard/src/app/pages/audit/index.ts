1b85b12aec93a9dce9711aa594176bb048bef14d
Alyx Mote <figments@outlook.com>
2020-10-10 10:34:28 -0700

    added barrel files

---
+++ packages/dashboard/src/app/pages/audit/index.ts
@@ -0,0 +1,1 @@
+export { AuditComponent } from './audit.component';
---
+++ packages/dashboard/src/app/pages/news/index.ts
@@ -0,0 +1,2 @@
+export { NewsComponent } from './news.component';
+export { PostFormComponent } from './post-form/post-form.component';
