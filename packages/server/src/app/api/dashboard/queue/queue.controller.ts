da892aa63e13b098c2c988f2e3778065d2f4319f
Alyx Mote <figments@outlook.com>
2020-10-10 15:10:05 -0700

    changed contribservice to queueservice

--- packages/server/src/app/api/dashboard/queue/queue.controller.ts
+++ packages/server/src/app/api/dashboard/queue/queue.controller.ts
@@ -2,40 +2,40 @@ import { Body, Controller, Request, Get, Param, Patch, Post, UseGuards } from '@
 import { Roles } from '@pulp-fiction/models/users';
 import { Decision } from '@pulp-fiction/models/contrib';
 import { RolesGuard } from '../../../guards';
-import { ContribService } from '../../contrib/contrib.service';
+import { QueueService } from './queue.service';
 
 @Controller('queue')
 export class QueueController {
-        constructor(private readonly contribService: ContribService) {}
+        constructor(private readonly queueService: QueueService) {}
 
         @UseGuards(RolesGuard([Roles.User]))
         @Post('submit-work/:workId')
         async submitWork(@Request() req: any, @Param('workId') workId: string) {
-            return await this.contribService.submitWork(req.user, workId);
+            return await this.queueService.submitWork(req.user, workId);
         }
     
         @UseGuards(RolesGuard([Roles.WorkApprover, Roles.Moderator, Roles.Admin]))
         @Get('queue/:pageNum')
         async getQueue(@Param('pageNum') pageNum: number) {
-            return await this.contribService.getQueue(pageNum);
+            return await this.queueService.getQueue(pageNum);
         }
     
         @UseGuards(RolesGuard([Roles.WorkApprover, Roles.Moderator, Roles.Admin]))
         @Get('queue-for-mod/:pageNum')
         async getQueueForMod(@Request() req: any, @Param('pageNum') pageNum: number) {
-            return await this.contribService.getQueueForMod(req.user, pageNum);
+            return await this.queueService.getQueueForMod(req.user, pageNum);
         }
     
         @UseGuards(RolesGuard([Roles.WorkApprover, Roles.Moderator, Roles.Admin]))
         @Patch('claim-work/:docId')
         async claimWork(@Request() req: any, @Param('docId') docId: string) {
-            return await this.contribService.claimWork(req.user, docId);
+            return await this.queueService.claimWork(req.user, docId);
         }
     
         @UseGuards(RolesGuard([Roles.WorkApprover, Roles.Moderator, Roles.Admin]))
         @Patch('approve-work')
         async approveWork(@Request() req: any, @Body() decision: Decision) {
-            return await this.contribService.approveWork(
+            return await this.queueService.approveWork(
                 req.user,
                 decision.docId,
                 decision.workId,
@@ -46,7 +46,7 @@ export class QueueController {
         @UseGuards(RolesGuard([Roles.WorkApprover, Roles.Moderator, Roles.Admin]))
         @Patch('reject-work')
         async rejectWork(@Request() req: any, @Body() decision: Decision) {
-            return await this.contribService.rejectWork(
+            return await this.queueService.rejectWork(
                 req.user,
                 decision.docId,
                 decision.workId,
