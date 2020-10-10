e49935cf1bc7f6ec4adcc9c29189718006385f2b
Alyx Mote <figments@outlook.com>
2020-10-10 13:32:33 -0700

    fixed another missing import

--- packages/server/src/app/api/dashboard/dashboard.module.ts
+++ packages/server/src/app/api/dashboard/dashboard.module.ts
@@ -5,10 +5,11 @@ import { DashboardService } from './dashboard.service';
 import { NewsController } from './news/news.controller';
 import { NewsModule } from '../../db/news';
 import { getJwtSecretKey, JWT_EXPIRATION } from '../../util';
+import { UsersModule } from '../../db/users/users.module';
 
 @Module({
   imports: [
-    NewsModule,
+    NewsModule, UsersModule,
     JwtModule.registerAsync({
       useFactory: () => ({
         secret: getJwtSecretKey(),
