9b86646cf73555bdd21829de1ee8e7cb311f3880
Alyx Mote <figments@outlook.com>
2020-10-10 13:31:06 -0700

    fixed missing import

--- packages/server/src/app/api/dashboard/dashboard.module.ts
+++ packages/server/src/app/api/dashboard/dashboard.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { DashboardService } from './dashboard.service';
 import { NewsController } from './news/news.controller';
 import { NewsModule } from '../../db/news';
+import { getJwtSecretKey, JWT_EXPIRATION } from '../../util';
 
 @Module({
-  imports: [NewsModule],
+  imports: [
+    NewsModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: getJwtSecretKey(),
+        signOptions: {expiresIn: JWT_EXPIRATION},
+      }),
+    }),
+  ],
   controllers: [NewsController],
   providers: [DashboardService]
 })
