537b9ad3936a72fadfcc4442530765e704049cf6
Alyx Mote <figments@outlook.com>
2020-10-10 19:57:20 -0700

    adding loading package

---
+++ packages/loading/src/lib/loading.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './loading/loading.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LoadingComponent],
+})
+export class LoadingModule {}
---
+++ packages/loading/src/lib/loading/loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pulp-fiction-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.less']
+})
+export class LoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
