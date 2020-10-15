398ed3150526e3cbebe26582370fdec320344a0b
Alyx Mote <figments@outlook.com>
2020-10-14 22:41:25 -0700

    added news feed resolver

--- packages/client/src/app/resolvers/index.ts
+++ packages/client/src/app/resolvers/index.ts
@@ -2,3 +2,4 @@ export { WorkPageResolver } from './work-page.resolver';
 export { BlogPageResolver } from './blog-page.resolver';
 export { PortfolioResolver } from './portfolio.resolver';
 export { PostPageResolver } from './post-page.resolver';
+export { NewsFeedResolver } from './news-feed.resolver';
