4a2cd61cb6bd5d5b3e55b45007e104a83f614638
SigmasonicX <sigmasonicx@gmail.com>
2020-10-14 19:20:42 -0700

    Change Title to namespace

--- packages/client/src/app/shared/title.ts
+++ packages/client/src/app/shared/title.ts
@@ -1,15 +1,15 @@
 import { Constants } from './global-constants';
 
-export class Title {
-    public static setDefaultTitle(): void {
+export namespace Title {
+    export function setDefaultTitle(): void {
         document.title = Constants.OFFPRINT;
     }
 
-    public static setTwoPartTitle(title: string): void {
+    export function setTwoPartTitle(title: string): void {
         document.title = title + Constants.SEPARATOR + Constants.OFFPRINT;
     }
 
-    public static setThreePartTitle(title1: string, title2: string): void {
+    export function setThreePartTitle(title1: string, title2: string): void {
         document.title = title1 + Constants.SEPARATOR + title2 +
             Constants.SEPARATOR + Constants.OFFPRINT;
     }
