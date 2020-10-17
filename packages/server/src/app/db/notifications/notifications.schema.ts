7a511b6ae67922a8fd08bacb633366d995582cc0
Alyx Mote <figments@outlook.com>
2020-10-16 22:23:17 -0700

    added database module for notifications

---
+++ packages/server/src/app/db/notifications/notifications.schema.ts
@@ -0,0 +1,9 @@
+import { Schema, Prop, SchemaFactory, raw } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { generate } from 'shortid';
+
+@Schema()
+export class NotificationsDocument extends Document {
+    @Prop({default: generate()})
+    readonly _id: string;
+}
