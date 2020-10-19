c47acddd9775e04676fb1b84e5942ca30253d97f
Alyx Mote <figments@outlook.com>
2020-10-18 22:18:30 -0700

    updated resolver

--- packages/client/src/app/resolvers/port-blogs.resolver.ts
+++ packages/client/src/app/resolvers/port-blogs.resolver.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { FrontendUser } from '@pulp-fiction/models/users';
 import { Observable, zip, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { PortBlogs } from '../models/site';
-import { PortfolioService } from '../services/content';
+import { AuthService } from '../services/auth';
+import { BlogsService, PortfolioService } from '../services/content';
 
 @Injectable()
 export class PortBlogsResolver implements Resolve<PortBlogs> {
+    currentUser: FrontendUser
     pageNum: number = 1;
 
-    constructor (private portService: PortfolioService) { }
+    constructor (private portService: PortfolioService, private blogsService: BlogsService, private authService: AuthService) {
+        this.authService.currUser.subscribe(x => {
+            this.currentUser = x;
+        });
+    }
 
     resolve(route: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): Observable<PortBlogs> {
         const userId = route.parent.paramMap.get('id');
@@ -20,15 +27,28 @@ export class PortBlogsResolver implements Resolve<PortBlogs> {
             this.pageNum = pageNum;
         }
 
-        const blogList = this.portService.getBlogList(userId, this.pageNum);
+        const blogsList = this.portService.getBlogList(userId, this.pageNum);
+        const userBlogs = this.blogsService.fetchUserBlogs(this.pageNum);
 
-        return zip(blogList, of(userId)).pipe(map(value => {
-            const portBlogs: PortBlogs = {
-                blogs: value[0],
-                userId: value[1]
-            };
+        if (this.currentUser) {
+            const userBlogsList = this.blogsService.fetchUserBlogs(this.pageNum);
 
-            return portBlogs;
-        }));
+            return zip(blogsList, userBlogsList).pipe(map(value => {
+                const portBlogs: PortBlogs = {
+                    blogs: value[0],
+                    userBlogs: value[1]
+                };
+    
+                return portBlogs;
+            }));
+        } else {
+            return zip(blogsList, of(null)).pipe(map(value => {
+                const portBlogs: PortBlogs = {
+                    blogs: value[0],
+                    userBlogs: value[1]
+                };
+                return portBlogs;
+            }));
+        }
     }
 }
