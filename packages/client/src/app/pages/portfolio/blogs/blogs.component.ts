5e943029a4e1f0f86a00d9f6991462b85ae0c61f
Alyx Mote <figments@outlook.com>
2020-10-18 22:31:15 -0700

    my blogs almost ported

--- packages/client/src/app/pages/portfolio/blogs/blogs.component.ts
+++ packages/client/src/app/pages/portfolio/blogs/blogs.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { FrontendUser } from '@pulp-fiction/models/users';
 import { Blog } from '@pulp-fiction/models/blogs';
 import { PaginateResult } from '@pulp-fiction/models/util';
 import { AuthService } from '../../../services/auth';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Constants, Title } from '../../../shared';
+import { PortBlogs } from '../../../models/site';
 
 @Component({
     selector: 'port-blogs',
@@ -19,6 +21,7 @@ export class BlogsComponent implements OnInit {
     userBlogsData: PaginateResult<Blog>
 
     pageNum = 1;
+    listView = false;
 
     searchBlogs = new FormGroup({
         query: new FormControl('')
@@ -30,7 +33,54 @@ export class BlogsComponent implements OnInit {
         });
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.portUser = this.route.parent.snapshot.data.portData as FrontendUser;
+        Title.setThreePartTitle(this.portUser.username, Constants.WORKS);
+
+        this.route.data.subscribe(data => {
+            const feedData = data.feedData as PortBlogs;
+            this.blogsData = feedData.blogs;
+            this.userBlogsData = feedData.userBlogs;
+        });
+    }
+
+    /**
+     * Handles page changing
+     * 
+     * @param event The new page
+     */
+    onPageChange(event: number) {
+        this.router.navigate([], {relativeTo: this.route, queryParams: {page: event}, queryParamsHandling: 'merge'});
+        this.pageNum = event;
+    }
+
+    /**
+     * Checks to see if the currently logged in user is the same as the user who owns this portfolio.
+     */
+    currentUserIsSame() {
+        if (this.currentUser) {
+            if (this.portUser) {
+                if (this.currentUser._id === this.portUser._id) {
+                    return true;
+                } else {
+                return false;
+                }
+            }
+        }
+    }
+
+    /**
+     * Switches the view
+     */
+    switchView() {
+        if (this.listView === true) {
+            this.listView = false;
+            this.router.navigate([], {relativeTo: this.route, queryParams: {page: 1}, queryParamsHandling: 'merge'});
+        } else {
+            this.listView = true;
+            this.router.navigate([], {relativeTo: this.route, queryParams: {page: 1}, queryParamsHandling: 'merge'});
+        }
+    }
 
     /**
      * Searches for a specific blog
