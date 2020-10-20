34be941dc2e3118d152870feed004780c98d9800
Alyx Mote <figments@outlook.com>
2020-10-20 14:50:31 -0700

    added some stuff to blogs content model

---
+++ packages/models/src/lib/content/blogs-content.model.ts
@@ -0,0 +1,10 @@
+import { ContentModel } from './content.model';
+
+export interface BlogsContentModel extends ContentModel {
+    audit: {
+        published: boolean;
+        publishedOn: Date;
+        hasComments: boolean;
+        isDeleted: boolean;
+    };
+}
