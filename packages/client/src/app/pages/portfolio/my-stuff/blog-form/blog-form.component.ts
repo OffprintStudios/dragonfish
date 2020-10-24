193f9c6b7b8323d5442ff655c0c17ca4436d4ac3
Alyx Mote <figments@outlook.com>
2020-10-23 23:21:36 -0700

    added some styles to the blog form

--- packages/client/src/app/pages/portfolio/my-stuff/blog-form/blog-form.component.ts
+++ packages/client/src/app/pages/portfolio/my-stuff/blog-form/blog-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { BlogForm } from '@pulp-fiction/models/content';
 
 @Component({
   selector: 'pulp-fiction-blog-form',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogFormComponent implements OnInit {
 
+  blogForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+    body: new FormControl('', [Validators.required, Validators.minLength(3)])
+  });
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  get fields() { return this.blogForm.controls; }
 }
