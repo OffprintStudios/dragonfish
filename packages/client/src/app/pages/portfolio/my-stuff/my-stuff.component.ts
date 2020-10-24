260a529129ef0037ec9b803aa7819b16b3f58e95
Alyx Mote <figments@outlook.com>
2020-10-24 00:05:16 -0700

    you can now navigate cleanly back to the stuff page

--- packages/client/src/app/pages/portfolio/my-stuff/my-stuff.component.ts
+++ packages/client/src/app/pages/portfolio/my-stuff/my-stuff.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { ContentModel } from '@pulp-fiction/models/content';
+import { FrontendUser } from '@pulp-fiction/models/users';
+import { AuthService } from '../../../services/auth';
 import { MyStuffService } from '../../../services/user';
 
 @Component({
@@ -11,13 +13,18 @@ import { MyStuffService } from '../../../services/user';
   styleUrls: ['./my-stuff.component.less']
 })
 export class MyStuffComponent implements OnInit {
+  currentUser: FrontendUser;
   myContent: ContentModel[];
 
   searchStuff = new FormGroup({
     query: new FormControl('')
   });
 
-  constructor(private stuffService: MyStuffService, public route: ActivatedRoute, private router: Router) { }
+  constructor(private stuffService: MyStuffService, public route: ActivatedRoute, private router: Router, private authService: AuthService) {
+    this.authService.currUser.subscribe(x => {
+      this.currentUser = x;
+    });
+  }
 
   ngOnInit(): void {
     this.myContent = this.route.snapshot.data.stuffData as ContentModel[];
