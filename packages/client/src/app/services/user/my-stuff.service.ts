64e97596cc023f1a30940059a89c99bbda69c732
Alyx Mote <figments@outlook.com>
2020-10-23 20:43:17 -0700

    began work on getting functions up and running

--- packages/client/src/app/services/user/my-stuff.service.ts
+++ packages/client/src/app/services/user/my-stuff.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyStuffService {
+  private url = `/api/content`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getContent() {
+    return this.http.get(`${this.url}/fetch-many?`)
+  }
 }
