1c0902688295e7f6263b6102e018ed3fee8ff4ba
Alyx Mote <figments@outlook.com>
2020-10-23 18:06:11 -0700

    added more stuff to the my stuff component

--- packages/client/src/app/pages/portfolio/my-stuff/my-stuff.component.ts
+++ packages/client/src/app/pages/portfolio/my-stuff/my-stuff.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'pulp-fiction-my-stuff',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-stuff.component.less']
 })
 export class MyStuffComponent implements OnInit {
+
+  searchStuff = new FormGroup({
+    query: new FormControl('')
+  });
 
   constructor() { }
 
