a9f8c2f89c9bc5444b3bf67b60bc0209c085bd1b
Alyx Mote <figments@outlook.com>
2020-10-23 22:12:49 -0700

    added some more stuff to component

--- packages/client/src/app/pages/portfolio/my-stuff/my-stuff.component.ts
+++ packages/client/src/app/pages/portfolio/my-stuff/my-stuff.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+import { ContentModel } from '@pulp-fiction/models/content';
+import { MyStuffService } from '../../../services/user';
+
 @Component({
   selector: 'pulp-fiction-my-stuff',
   templateUrl: './my-stuff.component.html',
   styleUrls: ['./my-stuff.component.less']
 })
 export class MyStuffComponent implements OnInit {
+  myContent: ContentModel[];
 
   searchStuff = new FormGroup({
     query: new FormControl('')
   });
 
-  constructor() { }
+  constructor(private stuffService: MyStuffService) { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
