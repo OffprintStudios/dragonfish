d6d1ec1be622d97133636253808335615414e9a3
Alyx Mote <figments@outlook.com>
2020-10-25 19:51:15 -0700

    added fetch one function

--- packages/client/src/app/services/user/my-stuff.service.ts
+++ packages/client/src/app/services/user/my-stuff.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
-import { ContentModel } from '@pulp-fiction/models/content';
+import { BlogsContentModel, ContentKind, ContentModel } from '@pulp-fiction/models/content';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +19,18 @@ export class MyStuffService {
       }), catchError(err => {
         return throwError(err);
       }));
+  }
+
+  public fetchOne(contentId: string, kind: ContentKind) {
+    if (kind === ContentKind.BlogContent) {
+      return this.http.get<BlogsContentModel>(`${this.url}/fetch-one?contentId=${contentId}&kind=${kind}`, {observe: 'response', withCredentials: true})
+        .pipe(map(res => {
+          return res.body;
+        }), catchError(err => {
+          return throwError(err);
+        }));
+    } else if (kind === ContentKind.WorkContent) {
+      // fetch work
+    }
   }
 }
