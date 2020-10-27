54b0dbe71cbe6ad4fe2790a6bee0a68c1c2ae6f4
Alyx Mote <figments@outlook.com>
2020-10-26 18:38:37 -0700

    added functions to mystuff service

--- packages/client/src/app/services/user/my-stuff.service.ts
+++ packages/client/src/app/services/user/my-stuff.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
-import { BlogsContentModel, ContentKind, ContentModel } from '@pulp-fiction/models/content';
+import { Types } from 'mongoose';
+import { BlogsContentModel, ContentKind, ContentModel, Folder, FolderForm } from '@pulp-fiction/models/content';
 
 @Injectable({
   providedIn: 'root'
@@ -43,7 +44,48 @@ export class MyStuffService {
       }));
   }
 
-  public addFolder() {
-    
+  public createFolder(folderInfo: FolderForm, parentId?: Types.ObjectId) {
+    if (parentId) {
+      return this.http.post<Folder>(`${this.url}/create-folder?parentId=${parentId}`, folderInfo, {observe: 'response', withCredentials: true})
+        .pipe(map(res => {
+          return res.body;
+        }), catchError(err => {
+          return throwError(err);
+        }));
+    } else {
+      return this.http.post<Folder>(`${this.url}/create-folder`, folderInfo, {observe: 'response', withCredentials: true})
+        .pipe(map(res => {
+          return res.body;
+        }), catchError(err => {
+          return throwError(err);
+        }));
+    }
+  }
+
+  public editFolder(folderInfo: FolderForm, folderId: Types.ObjectId) {
+    return this.http.patch<Folder>(`${this.url}/edit-folder?folderId=${folderId}`, folderInfo, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        return throwError(err);
+      }));
+  }
+
+  public fetchTopFolders() {
+    return this.http.get<Folder[]>(`${this.url}/fetch-top-folders`, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        return throwError(err);
+      }));
+  }
+
+  public fetchOneFolder(folderId: Types.ObjectId) {
+    return this.http.get<Folder>(`${this.url}/fetch-one-folder?folderId=${folderId}`, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        return throwError(err);
+      }));
   }
 }
