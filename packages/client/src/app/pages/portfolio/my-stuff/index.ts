95244ad181e15d329571a172766b3fe792f820be
Alyx Mote <figments@outlook.com>
2020-10-27 19:51:59 -0700

    removed folder page

--- packages/client/src/app/pages/portfolio/my-stuff/index.ts
+++ packages/client/src/app/pages/portfolio/my-stuff/index.ts
@@ -2,5 +2,4 @@ export { MyStuffComponent } from './my-stuff.component';
 export { BlogFormComponent } from './blog-form/blog-form.component';
 export { WorkFormComponent } from './work-form/work-form.component';
 export { NewFolderComponent } from './new-folder/new-folder.component';
-export { FolderPageComponent } from './folder-page/folder-page.component';
 export { ContentItemComponent } from './content-item/content-item.component';
