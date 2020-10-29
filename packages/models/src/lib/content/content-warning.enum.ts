e69c002cb8c0ce7ae9f23229768a026bb03bac12
Alyx Mote <figments@outlook.com>
2020-10-29 15:38:26 -0700

    added content rating and content warning enums

---
+++ packages/models/src/lib/content/content-warning.enum.ts
@@ -0,0 +1,10 @@
+export enum ContentWarning {
+    Abuse = 'AB',
+    AdultContent = 'AC',
+    Language = 'L',
+    Violence = 'V',
+    Nudity = 'N',
+    SexualContent = 'SC',
+    Rape = 'R',
+    MentalHealth = 'MH'
+}
--- packages/models/src/lib/content/index.ts
+++ packages/models/src/lib/content/index.ts
@@ -1,5 +1,7 @@
 export { ContentModel } from './content.model';
 export { ContentKind } from './content-kind.enum';
+export { ContentRating } from './content-rating.enum';
+export { ContentWarning } from './content-warning.enum';
 export { PubStatus } from './pub-status.enum';
 
 export * from './news';
