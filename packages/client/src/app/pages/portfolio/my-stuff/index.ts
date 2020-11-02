ba1fe8c5b3d18ac9eb06d6606f396b64d4ad7848
Alyx Mote <figments@outlook.com>
2020-11-02 14:32:51 -0800

    removed old work form

--- packages/client/src/app/pages/portfolio/my-stuff/index.ts
+++ packages/client/src/app/pages/portfolio/my-stuff/index.ts
@@ -1,6 +1,5 @@
 export { MyStuffComponent } from './my-stuff.component';
 export { BlogFormComponent } from './blog-form/blog-form.component';
-export { WorkFormComponent } from './work-form/work-form.component';
 export { ContentItemComponent } from './content-item/content-item.component';
 export { ProseFormComponent } from './prose-form/prose-form.component';
 export { PoetryFormComponent } from './poetry-form/poetry-form.component';
