8c0c91803ca26dbba14bc4144c4f3896bade28cf
Alyx Mote <figments@outlook.com>
2020-11-02 16:43:26 -0800

    added creation and editing functions for poetry and prose

--- packages/client/src/app/services/user/poetry.service.ts
+++ packages/client/src/app/services/user/poetry.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { PoetryContent, CreatePoetry } from '@pulp-fiction/models/content';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoetryService {
+  private url: `/api/content/poetry`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public createPoetry(poetryInfo: CreatePoetry) {
+    return this.http.put<PoetryContent>(`${this.url}/create-poetry`, poetryInfo, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        return throwError(err);
+      }));
+  }
+
+  public editPoetry(contentId: string, poetryInfo: CreatePoetry) {
+    return this.http.patch<void>(`${this.url}/edit-poetry?contentId=${contentId}`, poetryInfo, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        return throwError(err);
+      }));
+  }
 }
--- packages/client/src/app/services/user/prose.service.ts
+++ packages/client/src/app/services/user/prose.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { ProseContent, CreateProse } from '@pulp-fiction/models/content';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProseService {
+  private url: `/api/content/prose`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public createPoetry(proseInfo: CreateProse) {
+    return this.http.put<ProseContent>(`${this.url}/create-prose`, proseInfo, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        return throwError(err);
+      }));
+  }
+
+  public editPoetry(contentId: string, proseInfo: CreateProse) {
+    return this.http.patch<void>(`${this.url}/edit-prose?contentId=${contentId}`, proseInfo, {observe: 'response', withCredentials: true})
+      .pipe(map(res => {
+        return res.body;
+      }), catchError(err => {
+        return throwError(err);
+      }));
+  }
 }
