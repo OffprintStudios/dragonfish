55b4607c28117415d7e09d0fb1b3421248ab3a28
Alyx Mote <figments@outlook.com>
2020-11-06 01:31:35 -0800

    loads sections properly

--- packages/client/src/app/pages/portfolio/my-stuff/views/view-prose/view-prose.component.ts
+++ packages/client/src/app/pages/portfolio/my-stuff/views/view-prose/view-prose.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { ProseContent, PubStatus } from '@pulp-fiction/models/content';
+import { SectionsService } from 'packages/client/src/app/services/user';
+import { Section } from '@pulp-fiction/models/sections';
 
 @Component({
     selector: 'view-prose',
@@ -10,16 +12,26 @@ import { ProseContent, PubStatus } from '@pulp-fiction/models/content';
 })
 export class ViewProseComponent implements OnInit {
     myProse: ProseContent;
-
+    mySections: Section[];
     pubStatus = PubStatus;
+    loadingSections = false;
 
-    constructor(private route: ActivatedRoute, private location: Location) {}
+    constructor(private sectionsService: SectionsService, private route: ActivatedRoute, private location: Location) {}
 
     ngOnInit(): void {
         this.myProse = this.route.snapshot.data.proseData as ProseContent;
+        this.fetchData();
     }
 
     goBack() {
         this.location.back();
+    }
+
+    private fetchData() {
+        this.loadingSections = true;
+        this.sectionsService.fetchUserContentSections(this.myProse._id).subscribe(sections => {
+            this.mySections = sections;
+            this.loadingSections = false;
+        });
     }
 }
