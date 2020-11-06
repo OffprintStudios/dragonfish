f361c420fdb56eaf79bb5435f0de0427d7360cdd
Alyx Mote <figments@outlook.com>
2020-11-06 00:06:01 -0800

    removing old sections schema

--- packages/server/src/app/db/works/works.module.ts
+++ packages/server/src/app/db/works/works.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { UsersModule } from '../users/users.module';
 import { WorksSchema } from './works.schema';
-import { SectionsSchema } from './sections.schema';
 import { WorksService } from './works.service';
 import { HistoryModule } from '../history/history.module';
 
@@ -11,7 +10,6 @@ import { HistoryModule } from '../history/history.module';
   imports: [
     MongooseModule.forFeature([
       {name: 'Work', schema: WorksSchema},
-      {name: 'Section', schema: SectionsSchema},
     ]),
     UsersModule, HistoryModule
   ],
