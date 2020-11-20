ba20ab2e3222f0a7f1b551cd6f267c8ac6c2c401
Alyx Mote <figments@outlook.com>
2020-11-20 12:03:43 -0800

    add service routes for coverart uploading

--- packages/client/src/app/services/user/prose.service.ts
+++ packages/client/src/app/services/user/prose.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
+import { FileUploader, FileItem, ParsedResponseHeaders } from 'ng2-file-upload';
 
 import { ProseContent, CreateProse } from '@pulp-fiction/models/content';
+import { HttpError } from '../../models/site';
 
 @Injectable({
   providedIn: 'root'
@@ -29,5 +31,47 @@ export class ProseService {
       }), catchError(err => {
         return throwError(err);
       }));
+  }
+
+  public uploadCoverart(uploader: FileUploader) {
+    return new Observable<void>(observer => {
+      uploader.onCompleteItem = (_: FileItem, response: string, status: number, __: ParsedResponseHeaders) => {
+
+        if (status !== 201) {
+          const errorMessage: HttpError = this.tryParseJsonHttpError(response);
+          if (!errorMessage) {
+            const juryRiggedError: HttpError = {
+              statusCode: status, 
+              error: response
+            };
+            return observer.error(juryRiggedError);
+          } else  {
+            return observer.error(errorMessage);
+          }
+        }
+        
+        // If we ever need to retun the modified work, the return type on this
+        // should change to Observable<models.Work>, and we'd need to JSON parse
+        // the response and return it in .next() here.
+        observer.next()
+        observer.complete();
+      };      
+
+      uploader.uploadAll();
+    });   
+  }
+
+  /**
+   * Attempts to parse an HttpError.
+   * 
+   * @param response The response to parse
+   */
+  private tryParseJsonHttpError(response: string): HttpError | null {
+    try {
+      const error: HttpError = JSON.parse(response);
+      return error;
+    } catch (err) {
+      return null;
+    }
   }
 }
