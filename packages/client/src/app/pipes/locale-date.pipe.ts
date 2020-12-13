4f800777ca426f59244a91441260a2d1b048b314
SigmasonicX <sigmasonicx@gmail.com>
2020-12-13 12:26:53 -0800

    LocaleDatePipe now works

--- packages/client/src/app/pipes/locale-date.pipe.ts
+++ packages/client/src/app/pipes/locale-date.pipe.ts
@@ -2,8 +2,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'localedate'})
 export class LocaleDatePipe implements PipeTransform  {
-    transform(value: Date): string {
+    transform(value: Date | string): string {
+        const date = new Date(value);
         const options = { year: 'numeric', month: 'short', day: 'numeric' };
-        return value.toLocaleDateString(undefined, options);
+        return date.toLocaleDateString(undefined, options);
     }
 }
