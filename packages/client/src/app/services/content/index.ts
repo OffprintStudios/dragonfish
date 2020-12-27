cf1b0900c655ac55d717666fdffbf90dadf56efa
SigmasonicX <sigmasonicx@gmail.com>
2020-12-26 16:51:18 -0800

    First stage of removing unused files - Removes files related to old flows related to works

--- packages/client/src/app/services/content/index.ts
+++ packages/client/src/app/services/content/index.ts
@@ -2,7 +2,6 @@ export { BlogsService } from './blogs.service';
 export { PortfolioService } from './portfolio.service';
 export { WorksService } from './works.service';
 export { CollectionsService } from './collections.service';
-export { LocalSectionsService } from './local-sections.service';
 export { HistoryService } from './history.service';
 export { CommentsService } from './comments.service';
 export { MessagesService } from './messages.service';
