1cc815bea273b57aaac2f128516e98bc2a66906d
Alyx Mote <figments@outlook.com>
2021-01-12 18:11:06 -0800

    add getSiteStaff route to admin controller

--- packages/server/src/app/api/admin/admin.controller.ts
+++ packages/server/src/app/api/admin/admin.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { FrontendUser } from '@pulp-fiction/models/users';
+import { UsersService } from '../../db/users/users.service';
 
 @Controller()
 export class AdminController {
-    
+    constructor(private usersService: UsersService) {}
+
+    @Get('site-staff')
+    async getSiteStaff(): Promise<FrontendUser[]> {
+        return await this.usersService.getSiteStaff();
+    }
 }
