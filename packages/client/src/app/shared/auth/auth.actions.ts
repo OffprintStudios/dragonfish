2efe6439f5f7074daad12014af9808f49a87f78b
Alyx Mote <figments@outlook.com>
2021-01-17 17:00:48 -0800

    minor modifications

--- packages/client/src/app/shared/auth/auth.actions.ts
+++ packages/client/src/app/shared/auth/auth.actions.ts
@@ -3,11 +3,16 @@ import { LoginUser, CreateUser } from '@pulp-fiction/models/users';
 export namespace Auth {
     export class Login {
         static readonly type = '[Auth] Login';
-        constructor(public payload: LoginUser) {}
+        constructor (public payload: LoginUser) {}
     }
     
     export class Register {
         static readonly type = '[Auth] Register';
-        constructor(public payload: CreateUser) {}
+        constructor (public payload: CreateUser) {}
+    }
+
+    export class LogOut {
+        static readonly type = '[Auth] LogOut';
+        constructor () {}
     }
 }
