bab7100c0ddce06604e03571ee659a1d74320472
Alyx Mote <figments@outlook.com>
2021-01-18 16:28:18 -0800

    adding more stuff to user state

--- packages/client/src/app/shared/user/user.state.ts
+++ packages/client/src/app/shared/user/user.state.ts
@@ -4,6 +4,7 @@ import { tap } from 'rxjs/operators';
 
 import { User } from './user.actions';
 import { UserStateModel } from './user-state.model';
+import { UserService } from './services';
 
 @State<UserStateModel>({
     name: 'user',
@@ -13,5 +14,12 @@ import { UserStateModel } from './user-state.model';
 })
 @Injectable()
 export class UserState {
+    constructor (private userService: UserService) {}
 
+    @Action(User.SetUser)
+    setUser(ctx: StateContext<UserStateModel>, action: User.SetUser): void {
+        ctx.patchState({
+            currUser: action.user
+        });
+    }
 }
