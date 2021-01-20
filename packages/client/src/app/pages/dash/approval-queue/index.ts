21b5d7574f483f653c2b53f4fa45ca27a2c24b64
Alyx Mote <figments@outlook.com>
2021-01-20 03:18:55 -0800

    removed some stuff

--- packages/client/src/app/pages/dash/approval-queue/index.ts
+++ packages/client/src/app/pages/dash/approval-queue/index.ts
@@ -1,4 +1,3 @@
 export { ApprovalQueueComponent } from './approval-queue.component';
-export { ApprovalQueueService } from './approval-queue.service';
 export { ApprovalQueueResolver } from './approval-queue.resolver';
 export * from './views';
