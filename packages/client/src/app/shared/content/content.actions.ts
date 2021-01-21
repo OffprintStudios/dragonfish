9ac4e52a43c2e413dad915bb4f995ee59b3c44fa
Alyx Mote <figments@outlook.com>
2021-01-21 11:45:26 -0800

    update action names

--- packages/client/src/app/shared/content/content.actions.ts
+++ packages/client/src/app/shared/content/content.actions.ts
@@ -1,38 +1,38 @@
-import { ContentKind, SetRating } from "@pulp-fiction/models/content";
+import { ContentKind, SectionInfo, SetRating } from "@pulp-fiction/models/content";
 
 export namespace Content {
     export class FetchOne {
-        static readonly type = '[Content] FetchOne';
+        static readonly type = '[Content] Fetch One';
         constructor (public contentId: string, public kind: ContentKind) {}
     }
 
     export class FetchAll {
-        static readonly type = '[Content] FetchAll';
+        static readonly type = '[Content] Fetch All';
         constructor (public pageNum: number, public kinds: ContentKind[], public userId?: string) {}
     }
 
     export class SetSections {
-        static readonly type = '[Content] SetPubSections';
-        constructor () {}
+        static readonly type = '[Content] Set Sections';
+        constructor (public sections: SectionInfo[]) {}
     }
 
     export class FetchSection {
-        static readonly type = '[Content] FetchSection';
+        static readonly type = '[Content] Fetch Section';
         constructor (public sectionId: string) {}
     }
 
     export class SetLike {
-        static readonly type = '[Content] SetLike';
+        static readonly type = '[Content] Set Like';
         constructor (public setRating: SetRating) {}
     }
 
     export class SetDislike {
-        static readonly type = '[Content] SetDislike';
+        static readonly type = '[Content] Set Dislike';
         constructor (public setRating: SetRating) {}
     }
 
     export class SetNoVote {
-        static readonly type = '[Content] SetNoVote';
+        static readonly type = '[Content] Set No Vote';
         constructor (public setRating: SetRating) {}
     }
 }
--- packages/client/src/app/shared/dash/approval-queue/approval-queue.actions.ts
+++ packages/client/src/app/shared/dash/approval-queue/approval-queue.actions.ts
@@ -4,37 +4,37 @@ import { Decision } from "@pulp-fiction/models/contrib";
 
 export namespace AQNamespace {
     export class GetQueue {
-        static readonly type = '[ApprovalQueue] GetQueue';
+        static readonly type = '[ApprovalQueue] Get Queue';
         constructor (public pageNum: number) {}
     }
 
     export class GetQueueForMod {
-        static readonly type = '[ApprovalQueue] GetQueueForMod';
+        static readonly type = '[ApprovalQueue] Get Queue For Mod';
         constructor (public pageNum: number) {}
     }
 
     export class ClaimWork {
-        static readonly type = '[ApprovalQueue] ClaimWork';
+        static readonly type = '[ApprovalQueue] Claim Work';
         constructor (public doc: ApprovalQueue) {}
     }
 
     export class SelectWork {
-        static readonly type = '[ApprovalQueue] SelectWork';
+        static readonly type = '[ApprovalQueue] Select Work';
         constructor (public doc: ApprovalQueue | null) {}
     }
 
     export class ApproveWork {
-        static readonly type = '[ApprovalQueue] ApproveWork';
+        static readonly type = '[ApprovalQueue] Approve Work';
         constructor (public decision: Decision) {}
     }
 
     export class RejectWork {
-        static readonly type = '[ApprovalQueue] RejectWork';
+        static readonly type = '[ApprovalQueue] Reject Work';
         constructor (public decision: Decision) {}
     }
 
     export class ViewContent {
-        static readonly type = '[ApprovalQueue] ViewContent';
+        static readonly type = '[ApprovalQueue] View Content';
         constructor (public contentId: string, public kind: ContentKind, public userId: string) {}
     }
 }
