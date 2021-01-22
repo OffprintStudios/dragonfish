50d4bdadf5077d41d9f10e25ecf270f56e0d51e8
Alyx Mote <figments@outlook.com>
2021-01-21 20:52:39 -0800

    remove old paginateresult model

--- packages/models/src/lib/util/paginate-result.model.ts
+++ packages/models/src/lib/util/paginate-result.model.ts
@@ -1,17 +1,3 @@
-/*export interface PaginateResult<T> {
-    readonly docs: T[];
-    readonly totalDocs: number;
-    readonly limit: number;
-    readonly page?: number;
-    readonly totalPages: number;
-    readonly nextPage?: number;
-    readonly prevPage?: number;
-    readonly pagingCounter: number;
-    readonly hasPrevPage: boolean;
-    readonly hasNextPage: boolean;
-    readonly meta?: any;    
-}*/
-
 export class PaginateResult<T> {
     public docs: T[];
     public readonly totalDocs: number;
