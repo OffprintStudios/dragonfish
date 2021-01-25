0b80db3bf9437224aee6e2cef7fbd3f9a29519da
Alyx Mote <figments@outlook.com>
2021-01-24 23:20:55 -0800

    adding separate alerts module

---
+++ packages/alerts/src/lib/alerts.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+@NgModule({
+  imports: [CommonModule, MatSnackBarModule],
+})
+export class AlertsModule {}
