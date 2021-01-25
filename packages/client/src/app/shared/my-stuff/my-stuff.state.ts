5feb1806a9cdbbcbde5d10d5cfb8951d6a318be9
Alyx Mote <figments@outlook.com>
2021-01-25 04:24:49 -0800

    add actions

--- packages/client/src/app/shared/my-stuff/my-stuff.state.ts
+++ packages/client/src/app/shared/my-stuff/my-stuff.state.ts
@@ -1,18 +1,46 @@
 import { Injectable } from '@angular/core';
 import { State, Action, Selector, StateContext } from '@ngxs/store';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { MyStuff } from './my-stuff.actions';
 import { MyStuffStateModel } from './my-stuff-state.model';
+import { MyStuffService } from './services';
+import { AlertsState } from '../alerts';
+import { ContentModel } from '@pulp-fiction/models/content';
 
 @State<MyStuffStateModel>({
     name: 'myStuff',
     defaults: {
-        myStuff: null
+        myStuff: null,
+        currContent: null
     }
 })
 @Injectable()
 export class MyStuffState {
-    
+    constructor (private stuffService: MyStuffService) {}
+
+    @Action(MyStuff.SetFiles)
+    setFiles({ patchState }: StateContext<MyStuffStateModel>): Observable<ContentModel[]> {
+        return this.stuffService.fetchAll().pipe(tap((result: ContentModel[]) => {
+            patchState({
+                myStuff: result
+            });
+        }));
+    }
+
+    @Action(MyStuff.CreateContent)
+    createContent({ getState }: StateContext<MyStuffStateModel>) {
+        
+    }
+
+    @Action(MyStuff.SaveContent)
+    saveContent() {
+
+    }
+
+    @Action(MyStuff.UploadCoverArt)
+    uploadCoverArt() {
+
+    }
 }
