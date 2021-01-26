b4bb73ffe3c2bced06c03415944b557965833da4
Alyx Mote <figments@outlook.com>
2021-01-26 00:58:40 -0800

    removed unused routes

--- packages/server/src/app/api/content/prose/prose.controller.ts
+++ packages/server/src/app/api/content/prose/prose.controller.ts
@@ -14,22 +14,6 @@ export class ProseController {
     constructor(private readonly proseService: ProseService, private readonly imagesService: ImagesService) {}
 
     @UseGuards(RolesGuard([Roles.User]))
-    @Put('create-prose')
-    async createProse(@Request() req: any, @Body() proseInfo: CreateProse) {
-        return await this.proseService.createProse(req.user, proseInfo);
-    }
-
-    @UseGuards(RolesGuard([Roles.User]))
-    @Patch('edit-prose')
-    async editProse(@Request() req: any, @Query('contentId') contentId: string, @Body() proseInfo: CreateProse) {
-        if (isNullOrUndefined(contentId)) {
-            throw new BadRequestException(`The content ID is required.`);
-        }
-
-        return await this.proseService.editProse(req.user, contentId, proseInfo);
-    }
-
-    @UseGuards(RolesGuard([Roles.User]))
     @UseInterceptors(FileInterceptor('coverart'))
     @Post('upload-coverart/:proseId')
     async uploadCoverArt(@UploadedFile() coverArtImage: any, @Request() req: any, @Param('proseId') proseId: string) {
