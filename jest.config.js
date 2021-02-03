e9ee54ecaeb20d8dfd910bd4afb057f7b398241e
Alyx Mote <figments@outlook.com>
2021-02-03 15:55:39 -0800

    moved models and material to libraries root folder

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
     resolver: '@nrwl/jest/plugins/resolver',
     moduleFileExtensions: ['ts', 'js', 'html'],
     coverageReporters: ['html'],
-    projects: '<rootDir>/libraries/models',
+    projects: '<rootDir>/libraries/material',
 };
