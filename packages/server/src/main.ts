5ff72b18b5bafed693b96afc341550111ed8e5d1
Alyx Mote <figments@outlook.com>
2021-02-07 02:25:28 -0800

    re-add serve static

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -8,7 +8,6 @@ import * as csurf from 'csurf';
 import * as helmet from 'helmet';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
 /**
  * Determines the location of the required .env file.
@@ -25,21 +24,12 @@ async function bootstrap() {
     app.use(cookieParser(process.env.COOKIE_SECRET));
     app.use(json({ limit: '50mb' }));
     app.use(urlencoded({ limit: '50mb', extended: true }));
-    app.enableCors({
-        origin: process.env.ORIGIN,
-        credentials: true,
-        allowedHeaders: ['Accept', 'Authorization', 'Content-Type', 'Origin'],
-        methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-        optionsSuccessStatus: 204,
+    app.use(csurf({ cookie: { sameSite: 'none', secure: true } }));
+    // this is to make sure the XSRF-TOKEN is being set correctly
+    app.use(function (req, res, next) {
+        res.cookie('XSRF-TOKEN', req.csrfToken(), { sameSite: 'none', secure: true });
+        return next();
     });
-    if (process.env.NODE_ENV === 'production') {
-        app.use(csurf({ cookie: { sameSite: 'none', secure: true } }));
-        // this is to make sure the XSRF-TOKEN is being set correctly
-        app.use(function (req, res, next) {
-            res.cookie('XSRF-TOKEN', req.csrfToken(), { sameSite: 'none', secure: true });
-            return next();
-        });
-    }
     app.use(helmet());
     app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN));
     app.setGlobalPrefix('api');
@@ -47,15 +37,16 @@ async function bootstrap() {
     // Set up Swagger
     const config = new DocumentBuilder()
         .setTitle(`Dragonfish`)
-        .setDescription(`"Fire in its own way"--The official Offprint API.`)
+        .setDescription(`"Fire in its own way"&mdash;The official Offprint API.`)
         .setVersion(`1.0`)
         .addTag(`auth`)
         .addTag(`user`)
         .addTag(`content`)
         .addTag(`search`)
+        .addBearerAuth()
         .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api-docs', app, document);
+    SwaggerModule.setup('api', app, document);
 
     const port = process.env.PORT || 3333;
     await app.listen(port, () => {
