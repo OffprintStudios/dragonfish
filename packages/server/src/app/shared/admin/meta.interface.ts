75046ca99150a7a8c810f845cb2c8dffd4f9d20d
Alyx Mote <figments@outlook.com>
2021-02-07 14:12:41 -0800

    prettify things

--- packages/server/src/app/shared/admin/meta.interface.ts
+++ packages/server/src/app/shared/admin/meta.interface.ts
@@ -3,7 +3,7 @@ import { FrontendUser } from '@dragonfish/models/users';
 
 /**
  * ## The Meta Interface
- * 
+ *
  * Implement this interface to be able to serve data to associated routes.
  */
 export interface IMeta {
