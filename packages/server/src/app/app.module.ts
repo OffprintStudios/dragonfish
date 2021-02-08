312ac0885ee3434074dfc9c2050926aa764f3ef5
Alyx Mote <figments@outlook.com>
2021-02-08 01:24:42 -0800

    move providers to dedicated array

--- packages/server/src/app/app.module.ts
+++ packages/server/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NotificationsRoutes } from './controllers/notifications';
 import { SearchRoutes } from './controllers/search';
 
 /* Services */
+import { InterfaceProviders } from './services';
 import { AdminServices } from './services/admin';
 import { AuthServices } from './services/auth';
 import { ImagesServices } from './services/images';
@@ -83,72 +84,7 @@ import { getJwtSecretKey, JWT_EXPIRATION } from './util';
         ...ImagesServices,
         ...MigrationServices,
         ...SearchServices,
-        {
-            provide: 'IApprovalQueue',
-            useClass: AdminServices.find((x) => {
-                return x.name === 'ApprovalQueueService';
-            }),
-        },
-        {
-            provide: 'IMeta',
-            useClass: AdminServices.find((x) => {
-                return x.name === 'MetaService';
-            }),
-        },
-        {
-            provide: 'IAuth',
-            useClass: AuthServices.find((x) => {
-                return x.name === 'AuthService';
-            }),
-        },
-        {
-            provide: 'IUser',
-            useClass: AuthServices.find((x) => {
-                return x.name === 'UserService';
-            }),
-        },
-        {
-            provide: 'IImages',
-            useClass: ImagesServices.find((x) => {
-                return x.name === 'ImagesService';
-            }),
-        },
-        {
-            provide: 'IContent',
-            useClass: ContentServices.find((x) => {
-                return x.name === 'ContentService';
-            }),
-        },
-        {
-            provide: 'ISections',
-            useClass: ContentServices.find((x) => {
-                return x.name === 'SectionsService';
-            }),
-        },
-        {
-            provide: 'ICollections',
-            useClass: ContentServices.find((x) => {
-                return x.name === 'CollectionsService';
-            }),
-        },
-        {
-            provide: 'IComments',
-            useClass: ContentServices.find((x) => {
-                return x.name === 'CommentsService';
-            }),
-        },
-        {
-            provide: 'ISearch',
-            useClass: SearchServices.find((x) => {
-                return x.name === 'SearchService';
-            }),
-        },
-        {
-            provide: 'IHistory',
-            useClass: ContentServices.find((x) => {
-                return x.name === 'HistoryService';
-            }),
-        },
+        ...InterfaceProviders,
     ],
 })
 export class AppModule {}
