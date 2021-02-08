3d013cfcd061c755a35def4df3a1592b7bb50d28
Alyx Mote <figments@outlook.com>
2021-02-08 01:11:12 -0800

    adding history interface

--- packages/server/src/app/shared/content/index.ts
+++ packages/server/src/app/shared/content/index.ts
@@ -2,3 +2,4 @@ export { IContent } from './content.interface';
 export { ISections } from './sections.interface';
 export { ICollections } from './collections.interface';
 export { IComments } from './comments.interface';
+export { IHistory } from './history.interface';
