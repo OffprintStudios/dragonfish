2a9d364d16f0817e33d5ee6d8c8a80e6ca4b1c23
Alyx Mote <figments@outlook.com>
2021-02-08 19:48:02 -0800

    moved pipes over

---
+++ libraries/pipes/src/lib/pipes/locale-date.pipe.ts
@@ -0,0 +1,94 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'localedate' })
+export class LocaleDatePipe implements PipeTransform {
+    transform(value: Date | string, format: string): string {
+        const date = new Date(value);
+        var options: Intl.DateTimeFormatOptions;
+
+        switch (format) {
+            case 'short':
+                options = {
+                    year: '2-digit',
+                    month: 'numeric',
+                    day: 'numeric',
+                    hour12: true,
+                    hour: 'numeric',
+                    minute: '2-digit',
+                };
+                break;
+            case 'shortDate':
+                options = { year: '2-digit', month: 'numeric', day: 'numeric' };
+                break;
+            case 'shortTime':
+                options = { hour12: true, hour: 'numeric', minute: '2-digit' };
+                break;
+            case 'medium':
+                options = {
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric',
+                    hour12: true,
+                    hour: 'numeric',
+                    minute: '2-digit',
+                    second: '2-digit',
+                };
+                break;
+            case 'mediumDate':
+                options = { year: 'numeric', month: 'short', day: 'numeric' };
+                break;
+            case 'mediumTime':
+                options = { hour12: true, hour: 'numeric', minute: '2-digit', second: '2-digit' };
+                break;
+            case 'long':
+                options = {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour12: true,
+                    hour: 'numeric',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    timeZoneName: 'short',
+                };
+                break;
+            case 'longDate':
+                options = { year: 'numeric', month: 'long', day: 'numeric' };
+                break;
+            case 'longTime':
+                options = {
+                    hour12: true,
+                    hour: 'numeric',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    timeZoneName: 'short',
+                };
+                break;
+            case 'full':
+                options = {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour12: true,
+                    hour: 'numeric',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    timeZoneName: 'long',
+                };
+                break;
+            case 'fullDate':
+                options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+                break;
+            case 'fullTime':
+                options = { hour12: true, hour: 'numeric', minute: '2-digit', second: '2-digit', timeZoneName: 'long' };
+                break;
+        }
+
+        if (format === 'shortTime' || format === 'mediumTime' || format === 'longTime' || format === 'fullTime') {
+            return date.toLocaleTimeString(undefined, options);
+        } else {
+            return date.toLocaleDateString(undefined, options);
+        }
+    }
+}
---
+++ libraries/pipes/src/lib/pipes/separate-genres.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Genres } from '@dragonfish/models/content';
+
+@Pipe({ name: 'separateGenres' })
+export class SeparateGenresPipe implements PipeTransform {
+    transform(value: Genres[]) {
+        if (value.length === 1) {
+            return Genres[value[0]];
+        } else {
+            let theseGenres: string[] = new Array();
+            value.forEach((genre) => {
+                theseGenres.push(Genres[genre]);
+            });
+            return theseGenres.join(', ');
+        }
+    }
+}
