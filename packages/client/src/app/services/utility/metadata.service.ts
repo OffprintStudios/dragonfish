d0159642f99f1926f9b15aa5c904b3d9f9b85512
Alyx Mote <figments@outlook.com>
2021-02-08 20:07:28 -0800

    fixed SOME imports

--- packages/client/src/app/services/utility/metadata.service.ts
+++ packages/client/src/app/services/utility/metadata.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 
 export interface PageMetadata {
