66ff18c7917c8e6a50413d140712faca0e8e7d5c
Alyx Mote <figments@outlook.com>
2021-02-10 15:50:13 -0800

    changed service to use handleResponse

--- libraries/dashboard/src/lib/shared/approval-queue/services/approval-queue.service.ts
+++ libraries/dashboard/src/lib/shared/approval-queue/services/approval-queue.service.ts
@@ -8,6 +8,7 @@ import { ContentModel, ContentKind } from '@dragonfish/models/content';
 import { Section } from '@dragonfish/models/sections';
 import { ApprovalQueue } from '@dragonfish/models/approval-queue';
 import { Decision } from '@dragonfish/models/contrib';
+import { handleResponse } from '@dragonfish/utilities/functions';
 
 @Injectable()
 export class ApprovalQueueService {
@@ -19,24 +20,24 @@ export class ApprovalQueueService {
      * Gets the entire queue.
      */
     public getQueue(pageNum: number): Observable<PaginateResult<ApprovalQueue>> {
-      return this.http.get<PaginateResult<ApprovalQueue>>(`${this.url}/get-queue/${pageNum}`, {observe: 'response', withCredentials: true})
-        .pipe(map(entries => {
-          return entries.body;
-        }), catchError(err => {
-          return throwError(err);
-        }));
+        return handleResponse(
+            this.http.get<PaginateResult<ApprovalQueue>>(`${this.url}/get-queue/${pageNum}`, {
+                observe: 'response', 
+                withCredentials: true
+            }),
+        );
     }
   
     /**
      * Gets the claimed works from one moderator.
      */
     public getQueueForMod(pageNum: number): Observable<PaginateResult<ApprovalQueue>> {
-      return this.http.get<PaginateResult<ApprovalQueue>>(`${this.url}/get-queue-for-mod/${pageNum}`, {observe: 'response', withCredentials: true})
-        .pipe(map(entries => {
-          return entries.body;
-        }), catchError(err => {
-          return throwError(err);
-        }));
+        return handleResponse(
+            this.http.get<PaginateResult<ApprovalQueue>>(`${this.url}/get-queue-for-mod/${pageNum}`, {
+                observe: 'response', 
+                withCredentials: true
+            }),
+        );
     }
   
     /**
@@ -45,12 +46,12 @@ export class ApprovalQueueService {
      * @param docId The document to claim
      */
     public claimWork(docId: string): Observable<ApprovalQueue> {
-      return this.http.patch<ApprovalQueue>(`${this.url}/claim-work/${docId}`, {}, {observe: 'response', withCredentials: true})
-        .pipe(map(res => {
-          return res.body;
-        }), catchError(err => {
-          return throwError(err);
-        }))
+        return handleResponse(
+            this.http.patch<ApprovalQueue>(`${this.url}/claim-work/${docId}`, {}, {
+                observe: 'response', 
+                withCredentials: true,
+            }),
+        );
     }
   
     /**
@@ -59,12 +60,9 @@ export class ApprovalQueueService {
      * @param decision Info about the decision.
      */
     public approveWork(decision: Decision): Observable<void> {
-      return this.http.patch(`${this.url}/approve-work`, decision, {observe: 'response', withCredentials: true})
-        .pipe(map(() => {
-          return;
-        }), catchError(err => {
-          return throwError(err);
-        }));
+        return handleResponse(
+            this.http.patch<void>(`${this.url}/approve-work`, decision, {observe: 'response', withCredentials: true})
+        );
     }
   
     /**
@@ -73,12 +71,9 @@ export class ApprovalQueueService {
      * @param decision Info about the decision.
      */
     public rejectWork(decision: Decision): Observable<void> {
-      return this.http.patch(`${this.url}/reject-work`, decision, {observe: 'response', withCredentials: true})
-      .pipe(map(() => {
-        return;
-      }), catchError(err => {
-        return throwError(err);
-      }));
+        return handleResponse(
+            this.http.patch<void>(`${this.url}/reject-work`, decision, {observe: 'response', withCredentials: true})
+        );
     }
   
     /**
@@ -89,12 +84,12 @@ export class ApprovalQueueService {
      * @param userId The owner of the content
      */
     public viewContent(contentId: string, kind: ContentKind, userId: string): Observable<ContentModel> {
-      return this.http.get<ContentModel>(`${this.url}/view-content?contentId=${contentId}&kind=${kind}&userId=${userId}`, {observe: 'response', withCredentials: true})
-        .pipe(map(res => {
-          return res.body;
-        }), catchError(err => {
-          return throwError(err);
-        }))
+        return handleResponse(
+            this.http.get<ContentModel>(`${this.url}/view-content?contentId=${contentId}&kind=${kind}&userId=${userId}`, {
+                observe: 'response', 
+                withCredentials: true
+            }),
+        );
     }
   
     /**
@@ -103,11 +98,11 @@ export class ApprovalQueueService {
      * @param sectionId The section ID
      */
     public fetchSection(sectionId: string): Observable<Section> {
-      return this.http.get<Section>(`/api/content/sections/fetch-one-by-id?sectionId=${sectionId}&published=true`, {observe: 'response', withCredentials: true})
-        .pipe(map(res => {
-          return res.body;
-        }), catchError(err => {
-          return throwError(err);
-        }));
+        return handleResponse(
+            this.http.get<Section>(`/api/content/sections/fetch-one-by-id?sectionId=${sectionId}&published=true`, {
+                observe: 'response', 
+                withCredentials: true
+            }),
+        );
     }
 }
