1721702abbd52692529a8cbd68e328a7de8ea176
Alyx Mote <figments@outlook.com>
2021-02-10 13:54:28 -0800

    begin moving things over

---
+++ libraries/dashboard/src/lib/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'dash',
+    templateUrl: './dash.component.html',
+    styleUrls: ['./dash.component.less'],
+})
+export class DashComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
--- libraries/dashboard/src/lib/dashboard.module.ts
+++ libraries/dashboard/src/lib/dashboard.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { NgxsModule } from '@ngxs/store';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule.forChild([
-            /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-        ]),
+        DashboardRoutingModule,
+        NgxsModule.forFeature([])
     ],
 })
 export class DashboardModule {}
