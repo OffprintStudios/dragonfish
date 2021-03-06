a695f7335bc2f606856b85b3fd58889a94d9b7b9
PingZing <mcalistern@gmail.com>
2021-03-06 22:42:51 +0200

    Update deployment and provisioning steps to account for caddy and pm2

---
+++ deploy/pm2.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    apps : [{
+        name: 'dragonfish',
+        script: '../current_server/main.js',   
+        cwd: '../current_server/main.js'   
+    }],
+  };
--- deploy/provision.sh
+++ deploy/provision.sh
@@ -1,7 +1,14 @@
-# Provisions a machine for usage by the 'dragonfish' backend for Offprint.
+#!/bin/bash
 
-# Create the user that will be in charge of PM2 later
-sudo useradd dragonfish-user
+# Provisions a machine for usage by the 'dragonfish' backend for Offprint.
+hostname=$1
+if [[ -z $hostname ]]; then
+    echo "No hostname was included in the provisioning script. Terminating..."
+    exit 1
+fi
+
+# Create the dragonfish-cd user
+sudo useradd dragonfish-cd
 
 # Create the install directory
 sudo mkdir /opt/dragonfish
@@ -11,10 +18,9 @@ sudo mkdir /opt/dragonfish/releases
 sudo mkdir /opt/dragonfish/releases/server
 sudo mkdir /opt/dragonfish/releases/client
 
-# Set up group membership. Assumes that the `dragonfish-cd` user already exists.
+# Set up group membership.
 sudo groupapp dragonfishers
-sudo usermod -a -G dragonfishers dragonfish-cd
-sudo usermod -a -G dragonfishers dragonfish-user # <-- this will be the user in charge of actually running PM2
+sudo usermod -a -G dragonfishers dragonfish-cd # <-- this will be the user in charge of actually running PM2
 
 # Grant ownership of the install directory to the 'dragonfishers' group
 sudo chgrp -R dragonfishers /opt/dragonfish/
@@ -27,6 +33,16 @@ sudo chmod g+s /opt/dragonfish
 
 # Install and set up PM2
 sudo npm install pm2@4.5.4 -g
-# Redirect PM2's home, so it can be safely used by more than one user
-echo "PM2_HOME=\"/opt/dragonfish/.pm2\"" | sudo tee -a /etc/environment > /dev/null
-sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u dragonfish-user --hp /opt/dragonfish
+sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u dragonfish-cd --hp /home/dragonfish-cd
+pm2 start /opt/dragonfish/server_config/pm2.config.js
+
+# Install and set up caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
+caddy reverse-proxy --from $hostname --to localhost:3333
+
+# Once everything is set up, ensure that dragonfish-cd owns everything under /opt/dragonfish
+chown -R dragonfish-cd /opt/dragonfish/
