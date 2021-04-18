3d3538b268345d65b52bb4999f40e245f98f78ba
Alyx Mote <figments@outlook.com>
2021-04-18 14:03:51 -0700

    adding headers to messages and notifications

--- apps/bettafish/src/app/pages/messages/messages.component.ts
+++ apps/bettafish/src/app/pages/messages/messages.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'dragonfish-messages',
     templateUrl: './messages.component.html',
+    styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent {}
--- apps/bettafish/src/app/pages/notifications/notifications.component.ts
+++ apps/bettafish/src/app/pages/notifications/notifications.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'dragonfish-notifications',
     templateUrl: './notifications.component.html',
+    styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent {}
