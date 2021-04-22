884ef2122c66d1ab874bd554465ae4afc6c9c73e
Alyx Mote <figments@outlook.com>
2021-04-21 17:22:26 -0700

    moved portfolio to new design language

--- apps/bettafish/src/app/components/user/index.ts
+++ apps/bettafish/src/app/components/user/index.ts
@@ -1,3 +1,4 @@
 import { NotificationItemComponent } from './notification-item/notification-item.component';
+import { PortfolioToolbarComponent } from './portfolio-toolbar/portfolio-toolbar-component';
 
-export const UserComponents = [NotificationItemComponent];
+export const UserComponents = [NotificationItemComponent, PortfolioToolbarComponent];
