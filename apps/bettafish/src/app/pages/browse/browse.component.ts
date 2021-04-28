86dec9f9c86c78a5e4dcbe6c834ed4226125a22a
Alyx Mote <figments@outlook.com>
2021-04-28 13:40:02 -0700

    bring newest works page in line with rest of ux

--- apps/bettafish/src/app/pages/browse/browse.component.ts
+++ apps/bettafish/src/app/pages/browse/browse.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AlertsService } from '@dragonfish/client/alerts';
+import { ContentModel } from '@dragonfish/shared/models/content';
+import { NetworkService } from '../../services';
 
 @Component({
     selector: 'dragonfish-browse',
     templateUrl: './browse.component.html',
     styleUrls: ['./browse.component.scss']
 })
-export class BrowseComponent {
+export class BrowseComponent implements OnInit {
+    loadingNew = false;
+    newWorks: ContentModel[];
     searchForm = new FormGroup({
         query: new FormControl('')
     });
 
-    constructor(private route: ActivatedRoute, private router: Router, private alerts: AlertsService) {}
+    constructor(public route: ActivatedRoute, private router: Router, private alerts: AlertsService, private network: NetworkService) {}
+
+    ngOnInit() {
+        this.loadFirstNew();
+    }
+
+    loadFirstNew() {
+        this.loadingNew = true;
+        this.network.fetchFirstNew().subscribe(result => {
+            this.newWorks = result;
+            this.loadingNew = false;
+        }, () => {
+            this.loadingNew = false;
+        });
+    }
 
     submitSearch() {
         this.router.navigate(['search'], {
--- apps/bettafish/src/app/pages/browse/index.ts
+++ apps/bettafish/src/app/pages/browse/index.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { BrowseComponent } from './browse.component';
-import { TodayComponent } from './today/today.component';
 import { SearchComponent } from './search/search.component';
 import { NewestWorksComponent } from './newest-works/newest-works.component';
 
 export const BrowsePages = [
     BrowseComponent,
-    TodayComponent,
     SearchComponent,
     NewestWorksComponent,
 ];
@@ -16,11 +14,6 @@ export const BrowseRoutes: Routes = [
         path: 'browse',
         component: BrowseComponent,
         children: [
-            {
-                path: '',
-                component: TodayComponent,
-                pathMatch: 'full',
-            },
             {
                 path: 'search',
                 component: SearchComponent,
