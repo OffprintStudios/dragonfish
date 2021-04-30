1043cd1a5f5fc6f992bdfd3b9dc69fed2961fc87
Alyx Mote <figments@outlook.com>
2021-04-29 21:41:05 -0700

    history page fixes

--- libs/client/ui/src/lib/components/content-list-item/content-list-item.component.ts
+++ libs/client/ui/src/lib/components/content-list-item/content-list-item.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ContentKind } from '@dragonfish/shared/models/content';
 import { ListKind } from '@dragonfish/shared/models/util';
 import { calculateApprovalRating } from '@dragonfish/shared/functions';
+import { slugify } from 'voca';
 
 @Component({
     selector: 'dragonfish-content-list-item',
@@ -24,5 +25,21 @@ export class ContentListItemComponent {
      */
     calcApprovalRating(likes: number, dislikes: number) {
         return calculateApprovalRating(likes, dislikes);
+    }
+
+    /**
+     * Navigates to the selected content, specific to History.
+     */
+    navigateToContent(kind: ContentKind, id: string, title: string, userId: string, username: string) {
+        switch (kind) {
+            case ContentKind.PoetryContent:
+                return ['/poetry', id, slugify(title)];
+            case ContentKind.ProseContent:
+                return ['/prose', id, slugify(title)];
+            case ContentKind.NewsContent:
+                return ['/news', id, slugify(title)];
+            case ContentKind.BlogContent:
+                return ['/portfolio', userId, slugify(username), 'post', id, slugify(title)];
+        }
     }
 }
