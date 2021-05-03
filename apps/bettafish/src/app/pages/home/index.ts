267661161ba5045718a46f4b563d5b3cd1c28932
Alyx Mote <figments@outlook.com>
2021-05-03 09:24:53 -0700

    news feed should now display the correct page

--- apps/bettafish/src/app/pages/home/index.ts
+++ apps/bettafish/src/app/pages/home/index.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { NewsFeedComponent } from './news-feed/news-feed.component';
-import { NewsResolver } from '../../resolvers/home';
 
 export const HomePages = [HomeComponent, NewsFeedComponent];
 
@@ -11,7 +10,7 @@ export const HomeRoutes: Routes = [
         component: HomeComponent,
         pathMatch: 'full',
     },
-    { 
+    {
         path: 'home',
         redirectTo: '',
         pathMatch: 'full',
@@ -19,7 +18,5 @@ export const HomeRoutes: Routes = [
     {
         path: 'news',
         component: NewsFeedComponent,
-        resolve: { feedData: NewsResolver },
-        runGuardsAndResolvers: 'paramsChange',
     },
 ];
--- apps/bettafish/src/app/pages/home/news-feed/news-feed.component.ts
+++ apps/bettafish/src/app/pages/home/news-feed/news-feed.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PaginateResult } from '@dragonfish/shared/models/util';
-import { NewsContentModel, NewsCategory } from '@dragonfish/shared/models/content';
+import { NewsContentModel, NewsCategory, ContentKind } from '@dragonfish/shared/models/content';
 import { setTwoPartTitle, Constants} from '@dragonfish/shared/constants';
+import { untilDestroyed } from '@ngneat/until-destroy';
+import { NetworkService } from '../../../services';
 
 @Component({
     selector: 'dragonfish-news-feed',
@@ -13,14 +15,36 @@ export class NewsFeedComponent implements OnInit {
     posts: PaginateResult<NewsContentModel>;
     pageNum = 1;
     category = NewsCategory;
+    loading = false;
 
-    constructor(private route: ActivatedRoute, private router: Router) {}
+    constructor(private route: ActivatedRoute, private router: Router, private network: NetworkService) {}
 
     ngOnInit(): void {
-        this.route.data.subscribe((data) => {
-            this.posts = data.feedData;
+        this.route.queryParamMap.pipe(untilDestroyed(this)).subscribe(params => {
+            if (params.has('page')) {
+                this.pageNum = +params.get('page');
+            }
+            this.fetchData(this.pageNum);
         });
+
         setTwoPartTitle(Constants.NEWS);
+    }
+
+    /**
+     * Fetches data for the current page.
+     *
+     * @param pageNum the page to fetch
+     * @private
+     */
+    private fetchData(pageNum: number) {
+        this.loading = true;
+        this.network.fetchAllNew(pageNum, [ContentKind.NewsContent])
+            .subscribe(result => {
+                this.posts = result as PaginateResult<NewsContentModel>;
+                this.loading = false;
+            }, () => {
+                this.loading = false;
+            });
     }
 
     /**
