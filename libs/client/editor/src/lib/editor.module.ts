3120ddea5b9fb5803395f10ebc6fe831e2fa7500
Alyx Mote <figments@outlook.com>
2021-06-11 12:29:32 -0700

    removing ckeditor

--- libs/client/editor/src/lib/editor.module.ts
+++ libs/client/editor/src/lib/editor.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { EditorComponent } from './editor.component';
 
 @NgModule({
     declarations: [EditorComponent],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, CKEditorModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
     exports: [EditorComponent],
 })
 export class EditorModule {}
