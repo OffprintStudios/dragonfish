35987e1409d6884b6543ab6bf31548d9a87396d4
Alyx Mote <figments@outlook.com>
2021-07-20 16:42:56 -0700

    adding default component

---
+++ libs/client/my-library/src/lib/my-library.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dragonfish-my-library',
+    templateUrl: './my-library.component.html',
+    styleUrls: ['./my-library.component.scss'],
+})
+export class MyLibraryComponent {}
--- libs/client/my-library/src/lib/my-library.module.ts
+++ libs/client/my-library/src/lib/my-library.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyLibraryRoutingModule } from './my-library-routing.module';
 
+/* Pages */
+import { MyLibraryComponent } from './my-library.component';
+
 @NgModule({
+    declarations: [MyLibraryComponent],
     imports: [CommonModule, MyLibraryRoutingModule],
 })
 export class MyLibraryModule {}
