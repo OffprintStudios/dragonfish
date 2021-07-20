46577049975fc50c7ebea491b94c82bc6a20296a
Alyx Mote <figments@outlook.com>
2021-07-20 14:13:44 -0700

    adding my library module

---
+++ libs/client/my-library/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    displayName: 'client-my-library',
+    preset: '../../../jest.preset.js',
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.(html|svg)$',
+        },
+    },
+    coverageDirectory: '../../../coverage/libs/client/my-library',
+    transform: {
+        '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+    },
+    snapshotSerializers: [
+        'jest-preset-angular/build/serializers/no-ng-attributes',
+        'jest-preset-angular/build/serializers/ng-snapshot',
+        'jest-preset-angular/build/serializers/html-comment',
+    ],
+};
---
+++ libs/client/my-library/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/client-my-library.module';
