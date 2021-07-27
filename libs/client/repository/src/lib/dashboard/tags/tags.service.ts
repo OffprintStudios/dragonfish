947389a2a1f8a99717d25c9abff57b214ee1f47a
SigmasonicX <sigmasonicx@gmail.com>
2021-07-27 00:03:21 -0700

    Restores tagStore usage on Tags page

--- libs/client/repository/src/lib/dashboard/tags/tags.service.ts
+++ libs/client/repository/src/lib/dashboard/tags/tags.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { TagsStore } from './tags.store';
+import { LocalTagsStore } from './local-tags.store';
 import { DragonfishNetworkService } from '@dragonfish/client/services';
 import { AlertsService } from '@dragonfish/client/alerts';
 import { TagKind, TagsForm, TagsModel } from '@dragonfish/shared/models/content';
@@ -10,13 +10,16 @@ import { TagsTree } from '@dragonfish/shared/models/content/tags.model';
 @Injectable({ providedIn: 'root' })
 export class TagsService {
     constructor(
+        private tagsStore: LocalTagsStore,
         private network: DragonfishNetworkService,
         private alerts: AlertsService,
     ) {}
 
     public fetchTags(kind: TagKind): Observable<TagsModel[]> {
         return this.network.fetchTags(kind).pipe(
-            tap(() => {}),
+            tap(value => {
+                this.tagsStore.set(value);
+            }),
             catchError(err => {
                 this.alerts.error(err.error.message);
                 return throwError(() => err);
@@ -36,7 +39,9 @@ export class TagsService {
 
     public createTag(kind: TagKind, form: TagsForm): Observable<TagsModel> {
         return this.network.createTag(kind, form).pipe(
-            tap(() => {}),
+            tap(value => {
+                this.tagsStore.add(value);
+            }),
             catchError(err => {
                 this.alerts.error(err.error.message);
                 return throwError(() => err);
@@ -46,7 +51,9 @@ export class TagsService {
 
     public updateTag(id: string, form: TagsForm): Observable<TagsModel> {
         return this.network.updateTag(id, form).pipe(
-            tap(() => {}),
+            tap(value => {
+                this.tagsStore.update(id, value);
+            }),
             catchError(err => {
                 this.alerts.error(err.error.message);
                 return throwError(() => err);
@@ -57,6 +64,7 @@ export class TagsService {
     public deleteTag(id: string): Observable<void> {
         return this.network.deleteTag(id).pipe(
             tap(() => {
+                this.tagsStore.remove(id);
                 this.alerts.success(`Tag successfully deleted.`);
             }),
             catchError(err => {
