e7f92ca5c012df9e1edb7bfc2deece02a59b8108
SigmasonicX <sigmasonicx@gmail.com>
2021-08-05 22:53:33 -0700

    Removes unused section-document.model.ts

--- apps/api/src/app/db/works/models/index.ts
+++ apps/api/src/app/db/works/models/index.ts
@@ -1,2 +1,1 @@
-export { SectionDocument } from './section-document.model';
 export { WorkDocument } from './work-document.model';
--- apps/api/src/app/db/works/models/section-document.model.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-import { Section } from '@dragonfish/shared/models/works';
-
-export interface SectionDocument extends Section, Document {
-    readonly _id: string;
-}
