b4bf60966ec1e83d5b84b8132da4d268ad99b670
SigmasonicX <sigmasonicx@gmail.com>
2021-08-08 19:15:28 -0700

    Autopopulates parent in tag - Allows to fetch parent name for child tag

--- libs/api/database/src/lib/content/schemas/tags.schema.ts
+++ libs/api/database/src/lib/content/schemas/tags.schema.ts
@@ -17,8 +17,15 @@ export class TagsDocument extends Document implements TagsModel {
     @Prop({ trim: true, default: null })
     desc?: string;
 
-    @Prop({ type: String, default: null, ref: 'TagsDocument' })
-    parent?: string;
+    @Prop({
+        type: String,
+        default: null,
+        ref: 'Tags',
+        autopopulate: {
+            select: '_id name desc parent kind createdAt updatedAt',
+        },
+    })
+    parent?: string | TagsModel;
 
     @Prop({ type: String, enum: Object.keys(TagKind), required: true })
     readonly kind: TagKind;
