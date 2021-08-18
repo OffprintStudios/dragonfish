8764360cb72188eede1896dbaa4f30430438382d
SigmasonicX <sigmasonicx@gmail.com>
2021-08-18 00:05:58 -0700

    Sets tag page title, displays tag parent and links it - Removes extra spaces after ng-container tag

--- apps/bettafish/src/app/pages/tags/tag-page.component.ts
+++ apps/bettafish/src/app/pages/tags/tag-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { TagsQuery, TagsService } from "@dragonfish/client/repository/tags";
+import { setTwoPartTitle } from "@dragonfish/shared/constants";
+import { TagsModel } from "@dragonfish/shared/models/content";
 
 @Component({
     selector: 'dragonfish-tag-page',
@@ -19,7 +21,14 @@ export class TagPageComponent implements OnInit {
     ngOnInit(): void {
         this.route.paramMap.subscribe((params) => {
             this.tagId = params.get("tagId");
-            this.tagsService.fetchDescendants(this.tagId).subscribe();
+            this.tagsService.fetchDescendants(this.tagId).subscribe((tagsTree) => {
+                if (tagsTree.parent) {
+                    setTwoPartTitle((tagsTree.parent as TagsModel).name + " — " + tagsTree.name);
+                }
+                else {
+                    setTwoPartTitle(tagsTree.name);
+                }
+            });
         });
     }
 }
