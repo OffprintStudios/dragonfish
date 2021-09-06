83aabe3af24581928819ae201ab5708167e965b5
Alyx Mote <figments@outlook.com>
2021-09-06 13:36:45 -0700

    add blog page skeleton file

---
+++ libs/client/profile/src/lib/views/blog-page/blog-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dragonfish-blog-page',
+    templateUrl: './blog-page.component.html',
+    styleUrls: ['./blog-page.component.scss'],
+})
+export class BlogPageComponent {}
--- libs/client/profile/src/lib/views/index.ts
+++ libs/client/profile/src/lib/views/index.ts
@@ -1,5 +1,6 @@
 import { HomeComponent } from './home/home.component';
 import { WorksComponent } from './works/works.component';
 import { BlogsComponent } from './blogs/blogs.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
 
-export const Views = [HomeComponent, WorksComponent, BlogsComponent];
+export const Views = [HomeComponent, WorksComponent, BlogsComponent, BlogPageComponent];
