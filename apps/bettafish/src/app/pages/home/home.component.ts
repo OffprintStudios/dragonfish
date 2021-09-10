4ceec569e94dec0e2e300925ebedda254ecce38f
Alyx Mote <figments@outlook.com>
2021-09-10 16:48:51 -0700

    fixed performance issues on home page

--- apps/bettafish/src/app/pages/home/home.component.ts
+++ apps/bettafish/src/app/pages/home/home.component.ts
@@ -1,25 +1,21 @@
-import { AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Constants, setTwoPartTitle } from '@dragonfish/shared/constants';
 import { slogans } from '../../models/site';
 import { DragonfishNetworkService } from '@dragonfish/client/services';
 import { ActivatedRoute } from '@angular/router';
 import { NewsContentModel } from '@dragonfish/shared/models/content';
 import { delay } from 'rxjs/operators';
-import { NgScrollbar } from 'ngx-scrollbar';
-import { untilDestroyed } from '@ngneat/until-destroy';
 
 @Component({
     selector: 'dragonfish-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, AfterViewChecked {
-    @ViewChild('mainScroll') scrollbarRef: NgScrollbar;
+export class HomeComponent implements OnInit {
     rotatingSlogan = slogans[Math.floor(Math.random() * slogans.length)];
     siteVersion = Constants.SITE_VERSION;
     loadingLatest = false;
     latestPosts: NewsContentModel[];
-    transparent = true;
 
     constructor(private network: DragonfishNetworkService, public route: ActivatedRoute) {}
 
@@ -40,13 +36,5 @@ export class HomeComponent implements OnInit, AfterViewChecked {
                     this.loadingLatest = false;
                 },
             );
-    }
-
-    ngAfterViewChecked() {
-        if (this.scrollbarRef) {
-            this.scrollbarRef.scrolled.pipe(untilDestroyed(this)).subscribe((x) => {
-                this.transparent = x.target.scrollTop < 52;
-            });
-        }
     }
 }
