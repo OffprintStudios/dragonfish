3258f7a5698a110a1ee85f06fcdaec3e6ec2c7fa
Alyx Mote <figments@outlook.com>
2021-09-27 10:24:01 -0700

    working on the draft blog page

--- libs/client/profile/src/lib/views/draft-blog-page/draft-blog-page.component.ts
+++ libs/client/profile/src/lib/views/draft-blog-page/draft-blog-page.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogsContentModel } from '@dragonfish/shared/models/content';
+import { map } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
     selector: 'dragonfish-draft-blog-page',
     templateUrl: './draft-blog-page.component.html',
     styleUrls: ['./draft-blog-page.component.scss'],
 })
-export class DraftBlogPageComponent {}
+export class DraftBlogPageComponent implements OnInit {
+    blog: BlogsContentModel;
+
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        this.route.data
+            .pipe(
+                map((content) => {
+                    this.blog = content as BlogsContentModel;
+                }),
+                untilDestroyed(this),
+            )
+            .subscribe();
+    }
+}
