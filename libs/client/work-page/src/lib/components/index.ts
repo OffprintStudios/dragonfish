f1ce9a0f3f2b2d3ac7e3b02228c3141c91b817d2
Alyx Mote <figments@outlook.com>
2021-09-30 15:03:02 -0700

    sections list skeleton files

--- libs/client/work-page/src/lib/components/index.ts
+++ libs/client/work-page/src/lib/components/index.ts
@@ -1,4 +1,5 @@
 import { WorkBannerComponent } from './work-banner/work-banner.component';
 import { UploadCoverArtComponent } from './upload-cover-art/upload-cover-art.component';
+import { SectionsListComponent } from './sections-list/sections-list.component';
 
-export const Components = [WorkBannerComponent, UploadCoverArtComponent];
+export const Components = [WorkBannerComponent, UploadCoverArtComponent, SectionsListComponent];
---
+++ libs/client/work-page/src/lib/components/sections-list/sections-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dragonfish-sections-list',
+    templateUrl: './sections-list.component.html',
+    styleUrls: ['./sections-list.component.scss'],
+})
+export class SectionsListComponent {}
