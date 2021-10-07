716fb1fa50361dcb8c8331cc27f347db9e43ca7f
Alyx Mote <figments@outlook.com>
2021-10-07 14:39:16 -0700

    Update comments.state.ts

--- libs/client/repository/src/lib/comments/comments.state.ts
+++ libs/client/repository/src/lib/comments/comments.state.ts
@@ -1,4 +1,4 @@
 import { EntityState } from '@datorama/akita';
 import { Comment } from '@dragonfish/shared/models/comments';
 
-export interface CommentsState extends EntityState<Comment, string> {}
+export type CommentsState = EntityState<Comment, string>;
