44993288107e3e01705b57e8b37b06ebe90b05a3
Alyx Mote <figments@outlook.com>
2021-10-06 22:16:07 -0700

    readd fandom tags

--- libs/client/work-page/src/lib/views/content-info/content-info.component.ts
+++ libs/client/work-page/src/lib/views/content-info/content-info.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { WorkPageQuery, WorkPageService } from '@dragonfish/client/repository/work-page';
 import { RatingOption } from '@dragonfish/shared/models/reading-history';
 import { SessionQuery } from '@dragonfish/client/repository/session';
+import { AuthService } from '@dragonfish/client/repository/session/services';
 
 @Component({
     selector: 'dragonfish-content-info',
@@ -11,5 +12,10 @@ import { SessionQuery } from '@dragonfish/client/repository/session';
 export class ContentInfoComponent {
     ratingOption = RatingOption;
 
-    constructor(public workQuery: WorkPageQuery, public workPage: WorkPageService, public session: SessionQuery) {}
+    constructor(
+        public workQuery: WorkPageQuery,
+        public workPage: WorkPageService,
+        public session: SessionQuery,
+        public auth: AuthService,
+    ) {}
 }
