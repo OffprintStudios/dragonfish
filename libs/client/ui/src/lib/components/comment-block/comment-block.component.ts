50a7b2e3eb25fc7ec494bb8558b2b2e8f2b0648b
Alyx Mote <figments@outlook.com>
2021-10-07 19:27:43 -0700

    editing comments is in

--- libs/client/ui/src/lib/components/comment-block/comment-block.component.ts
+++ libs/client/ui/src/lib/components/comment-block/comment-block.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Comment, CommentForm, CommentKind } from '@dragonfish/shared/models/comments';
 import { PseudonymsQuery } from '@dragonfish/client/repository/pseudonyms';
 import { CommentsService } from '@dragonfish/client/repository/comments';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertsService } from '@dragonfish/client/alerts';
+import { AuthService } from '@dragonfish/client/repository/session/services';
+import { SessionQuery } from '@dragonfish/client/repository/session';
 
 @Component({
     selector: 'dragonfish-comment-block',
     templateUrl: './comment-block.component.html',
     styleUrls: ['./comment-block.component.scss'],
 })
-export class CommentBlockComponent {
+export class CommentBlockComponent implements OnInit {
     @Input() createMode = false;
     @Input() comment: Comment;
     @Input() itemId: string;
@@ -24,9 +26,19 @@ export class CommentBlockComponent {
 
     constructor(
         public pseudQuery: PseudonymsQuery,
+        public auth: AuthService,
+        public session: SessionQuery,
         private commentsService: CommentsService,
         private alerts: AlertsService,
     ) {}
+
+    ngOnInit(): void {
+        if (this.comment && this.createMode === false) {
+            this.commentForm.setValue({
+                body: this.comment.body,
+            });
+        }
+    }
 
     toggleEditMode = () => {
         this.editMode = !this.editMode;
