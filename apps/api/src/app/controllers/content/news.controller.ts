6b4d5e8b9a3096d1d01cdb36dec59742db60b10d
Alyx Mote <figments@outlook.com>
2021-10-11 17:39:13 -0700

    updating backend routes for using blogposts as newsposts

--- apps/api/src/app/controllers/content/news.controller.ts
+++ apps/api/src/app/controllers/content/news.controller.ts
@@ -1,10 +1,5 @@
-import { Controller, Param, Get, UseGuards } from '@nestjs/common';
-import { Cookies } from '@nestjsplus/cookies';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ContentGroupStore } from '@dragonfish/api/database/content/stores';
-import { OptionalAuthGuard } from '../../guards';
-import { ContentFilter, ContentKind } from '@dragonfish/shared/models/content';
-import { User } from '@dragonfish/api/utilities/decorators';
-import { JwtPayload } from '@dragonfish/shared/models/auth';
 
 @Controller('news')
 export class NewsController {
@@ -15,18 +10,8 @@ export class NewsController {
         return await this.contentGroupStore.fetchForHome();
     }
 
-    @Get('news-feed/:pageNum')
-    async getNewsFeed(@Param('pageNum') pageNum: number, @Cookies('contentFilter') filter: ContentFilter) {
-        return await this.contentGroupStore.fetchAllPublished(pageNum, [ContentKind.NewsContent], filter);
-    }
-
-    @UseGuards(OptionalAuthGuard)
-    @Get('news-post/:postId')
-    async getNewsPost(@User() user: JwtPayload, @Param('postId') postId: string) {
-        return await this.contentGroupStore.fetchOnePublished(
-            postId, // the contentId of the post
-            ContentKind.NewsContent, // fetch only a news post
-            user, // add a view if there's a user
-        );
+    @Get('news-feed')
+    async getNewsFeed(@Query('page') page: number) {
+        return await this.contentGroupStore.fetchNewsFeed(page);
     }
 }
