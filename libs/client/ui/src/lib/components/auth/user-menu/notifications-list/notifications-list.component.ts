087ac45ac29e08f32d6b17fe54c4e1ebe2632b13
Alyx Mote <figments@outlook.com>
2021-10-17 20:45:24 -0700

    adding proper links

--- libs/client/ui/src/lib/components/auth/user-menu/notifications-list/notifications-list.component.ts
+++ libs/client/ui/src/lib/components/auth/user-menu/notifications-list/notifications-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NotificationsQuery, NotificationsService } from '@dragonfish/client/repository/notifications';
 import { NotificationKind } from '@dragonfish/shared/models/accounts/notifications';
+import { ContentKind } from '@dragonfish/shared/models/content';
+import { slugify } from 'voca';
 
 @Component({
     selector: 'dragonfish-notifications-list',
@@ -22,5 +24,29 @@ export class NotificationsListComponent {
 
     removeFromActive(id: string) {
         this.notificationsService.removeFromActive(id);
+    }
+
+    createProfileLink(commentInfo) {
+        return ['/profile', commentInfo.posterId, commentInfo.posterTag];
+    }
+
+    createContentLink(contentInfo, commentInfo) {
+        switch (contentInfo.contentKind as ContentKind) {
+            case ContentKind.BlogContent:
+                return [
+                    '/profile',
+                    commentInfo.posterId,
+                    commentInfo.posterTag,
+                    'post',
+                    contentInfo.contentId,
+                    slugify(contentInfo.contentTitle),
+                ];
+            case ContentKind.PoetryContent:
+                return ['/poetry', contentInfo.contentId, slugify(contentInfo.contentTitle)];
+            case ContentKind.ProseContent:
+                return ['/prose', contentInfo.contentId, slugify(contentInfo.contentTitle)];
+            default:
+                return [];
+        }
     }
 }
