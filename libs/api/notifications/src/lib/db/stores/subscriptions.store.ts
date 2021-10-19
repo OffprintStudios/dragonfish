584a3fe79971a76577983ffb219ea09e9510cad1
Alyx Mote <figments@outlook.com>
2021-10-19 12:54:54 -0700

    Update subscriptions.store.ts

--- libs/api/notifications/src/lib/db/stores/subscriptions.store.ts
+++ libs/api/notifications/src/lib/db/stores/subscriptions.store.ts
@@ -8,6 +8,14 @@ import { SubscriptionPayload } from '@dragonfish/shared/models/accounts/notifica
 export class SubscriptionsStore {
     constructor(@InjectModel('Subscription') private readonly subscription: Model<SubscriptionDocument>) {}
 
+    //#region ---FETCHING---
+
+    public async fetchAll(pseudId: string) {
+        return this.subscription.find({ subscriberId: pseudId });
+    }
+
+    //#endregion
+
     //#region ---CRUD OPERATIONS---
 
     public async create(payload: SubscriptionPayload): Promise<SubscriptionDocument> {
@@ -18,6 +26,10 @@ export class SubscriptionsStore {
         });
 
         return await newSub.save();
+    }
+
+    public async delete(pseudId: string, subId: string): Promise<void> {
+        await this.subscription.deleteOne({ _id: subId, subscriberId: pseudId });
     }
 
     //#endregion
