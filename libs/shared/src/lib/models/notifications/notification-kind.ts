7026fb579b55158bb83a21617e0bb63102bdaf70
Alyx Mote <figments@outlook.com>
2021-10-19 09:27:16 -0700

    remove old notifications stuff

--- libs/shared/src/lib/models/notifications/notification-kind.ts
+++
@@ -1,9 +0,0 @@
-export enum NotificationKind {
-    WorkNotification = 'WorkNotification',
-    SectionNotification = 'SectionNotification',
-    BlogNotification = 'BlogNotification',
-    CommentNotification = 'CommentNotification',
-    NewsPostNotification = 'NewsPostNotification',
-    PMThreadNotification = 'PMThreadNotification',
-    PMReplyNotification = 'PMReplyNotification',
-}
