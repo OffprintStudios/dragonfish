cc1442d007da10e149d394210661aa86ee4413b1
Alyx Mote <figments@outlook.com>
2021-10-19 14:03:14 -0700

    move libraries over to pseuds in the api

--- apps/api/src/app/controllers/content-library/content-library.controller.ts
+++ apps/api/src/app/controllers/content-library/content-library.controller.ts
@@ -1,27 +1,29 @@
 import { Controller, UseGuards, Get, Put, Query, Delete } from '@nestjs/common';
-import { RolesGuard } from '../../guards';
-import { Roles } from '@dragonfish/shared/models/users';
+import { Roles } from '@dragonfish/shared/models/accounts';
 import { ContentLibraryStore } from '@dragonfish/api/database/content-library/stores';
-import { User } from '@dragonfish/api/utilities/decorators';
-import { JwtPayload } from '@dragonfish/shared/models/auth';
+import { Identity, User } from '@dragonfish/api/utilities/decorators';
+import { IdentityGuard } from '@dragonfish/api/utilities/guards';
 
-@UseGuards(RolesGuard([Roles.User]))
+@UseGuards(IdentityGuard)
 @Controller('content-library')
 export class ContentLibraryController {
     constructor(private readonly libraryStore: ContentLibraryStore) {}
 
+    @Identity(Roles.User)
     @Get('fetch')
-    public async fetchLibrary(@User() user: JwtPayload) {
-        return await this.libraryStore.fetchLibrary(user);
+    public async fetchLibrary(@Query('pseudId') pseudId: string) {
+        return await this.libraryStore.fetchLibrary(pseudId);
     }
 
+    @Identity(Roles.User)
     @Put('add-to')
-    public async addToLibrary(@User() user: JwtPayload, @Query('contentId') contentId: string) {
-        return await this.libraryStore.addToLibrary(user, contentId);
+    public async addToLibrary(@Query('pseudId') pseudId: string, @Query('contentId') contentId: string) {
+        return await this.libraryStore.addToLibrary(pseudId, contentId);
     }
 
+    @Identity(Roles.User)
     @Delete('remove')
-    public async removeFromLibrary(@User() user: JwtPayload, @Query('contentId') contentId: string) {
-        return await this.libraryStore.removeFromLibrary(user, contentId);
+    public async removeFromLibrary(@Query('pseudId') pseudId: string, @Query('contentId') contentId: string) {
+        return await this.libraryStore.removeFromLibrary(pseudId, contentId);
     }
 }
