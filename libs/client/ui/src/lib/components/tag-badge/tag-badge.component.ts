67a3fbb208705955b6943a9e9679e6d928ffb6cb
Alyx Mote <figments@outlook.com>
2021-10-22 12:20:39 -0700

    fixing up ratings icons

--- libs/client/ui/src/lib/components/tag-badge/tag-badge.component.ts
+++ libs/client/ui/src/lib/components/tag-badge/tag-badge.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { ContentKind, Genres, TagKind, WorkKind } from '@dragonfish/shared/models/content';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { ContentKind, ContentRating, Genres, TagKind, WorkKind } from '@dragonfish/shared/models/content';
 
 @Component({
     selector: 'dragonfish-tag-badge',
@@ -7,17 +7,62 @@ import { ContentKind, Genres, TagKind, WorkKind } from '@dragonfish/shared/model
     styleUrls: ['./tag-badge.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
-export class TagBadgeComponent {
+export class TagBadgeComponent implements OnInit {
     @Input() kind: TagKind;
     @Input() type: ContentKind;
     @Input() category: WorkKind;
     @Input() genre: Genres;
-    @Input() size: 'small' | 'large' = 'large';
+    @Input() rating: ContentRating;
+    @Input() size: 'small' | 'medium' | 'large' = 'medium';
+    @Input() hasIcon = true;
 
     types = ContentKind;
     categories = WorkKind;
     kinds = TagKind;
     genres = Genres;
+
+    /* Ratings */
+    ratings = ContentRating;
+    isEveryone = false;
+    isTeen = false;
+    isMature = false;
+    isExplicit = false;
+
+    ngOnInit(): void {
+        if (this.kind === TagKind.Rating) {
+            if (this.rating === ContentRating.Everyone) {
+                this.isEveryone = true;
+                this.isTeen = false;
+                this.isMature = false;
+                this.isExplicit = false;
+            } else if (this.rating === ContentRating.Teen) {
+                this.isEveryone = false;
+                this.isTeen = true;
+                this.isMature = false;
+                this.isExplicit = false;
+            } else if (this.rating === ContentRating.Mature) {
+                this.isEveryone = false;
+                this.isTeen = false;
+                this.isMature = true;
+                this.isExplicit = false;
+            } else if (this.rating === ContentRating.Explicit) {
+                this.isEveryone = false;
+                this.isTeen = false;
+                this.isMature = false;
+                this.isExplicit = true;
+            } else {
+                this.isEveryone = false;
+                this.isTeen = false;
+                this.isMature = false;
+                this.isExplicit = false;
+            }
+        } else {
+            this.isEveryone = false;
+            this.isTeen = false;
+            this.isMature = false;
+            this.isExplicit = false;
+        }
+    }
 
     generateTooltip() {
         switch (this.kind) {
@@ -63,6 +108,18 @@ export class TagBadgeComponent {
                         return 'Slice of Life';
                     case Genres.SpeculativeFiction:
                         return 'Speculative Fiction';
+                }
+                break;
+            }
+            case TagKind.Rating: {
+                if (this.rating === ContentRating.Everyone) {
+                    return 'Everyone';
+                } else if (this.rating === ContentRating.Teen) {
+                    return 'Teen';
+                } else if (this.rating === ContentRating.Mature) {
+                    return 'Mature';
+                } else if (this.rating === ContentRating.Explicit) {
+                    return 'Explicit';
                 }
             }
         }
