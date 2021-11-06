44502a43ddc22371fa4b8b539eb755bfb84bf18d
SigmasonicX <sigmasonicx@gmail.com>
2021-11-06 15:32:01 -0700

    Adds links and number of tagged works to tags page - Removes unused import from display-tags.pipe.ts

--- libs/client/pipes/src/lib/pipes/display-tags.pipe.ts
+++ libs/client/pipes/src/lib/pipes/display-tags.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Genres, TagsModel } from '@dragonfish/shared/models/content';
+import { TagsModel } from '@dragonfish/shared/models/content';
 
 @Pipe({ name: 'displayTags' })
 export class DisplayTagsPipe implements PipeTransform {
