4ad35d910e30e2400407b958911b01b42b45be04
SigmasonicX <sigmasonicx@gmail.com>
2021-11-07 22:37:34 -0800

    Sorts tags into sections, with links to each section

--- apps/bettafish/src/app/pages/browse/fandom-tags/fandom-tags.component.ts
+++ apps/bettafish/src/app/pages/browse/fandom-tags/fandom-tags.component.ts
@@ -3,6 +3,7 @@ import { UntilDestroy } from '@ngneat/until-destroy';
 import { Constants, setTwoPartTitle } from '@dragonfish/shared/constants';
 import { TagsQuery, TagsService } from '@dragonfish/client/repository/tags';
 import { TagKind } from '@dragonfish/shared/models/content';
+import { AlphabeticalTags } from './alphabetical-tags.component';
 
 @UntilDestroy()
 @Component({
@@ -11,14 +12,36 @@ import { TagKind } from '@dragonfish/shared/models/content';
     styleUrls: ['./fandom-tags.component.scss'],
 })
 export class FandomTagsComponent implements OnInit {
+    alphabeticalTags: AlphabeticalTags[] = [];
     constructor(
         public tagsQuery: TagsQuery,
         private tagsService: TagsService,
     ) { }
 
     ngOnInit(): void {
-        this.tagsService.fetchTagsTrees(TagKind.Fandom).subscribe();
-
         setTwoPartTitle(Constants.FANDOM_TAGS);
+
+        // Assumes alphabetical sorting for tags
+        this.tagsService.fetchTagsTrees(TagKind.Fandom).subscribe((tags) => {
+            let savedLetter: string;
+            let currentAlphaTags: AlphabeticalTags;
+            for (const tag of tags) {
+                // If first letter doesn't match saved letter, save letter and move to new section
+                // Then either way, add to that letter's array
+                if (savedLetter !== tag.name[0]) {
+                    if (currentAlphaTags) {
+                        this.alphabeticalTags.push(currentAlphaTags);
+                    }
+                    savedLetter = tag.name[0];
+                    currentAlphaTags = new AlphabeticalTags(tag.name[0]);
+                }
+                currentAlphaTags.tags.push(tag);
+            }
+            // Add last set
+            if (currentAlphaTags) {
+                this.alphabeticalTags.push(currentAlphaTags);
+            }
+        });
     }
+
 }
