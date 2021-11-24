ca85da0ec8008f753706f7a07e67dd0bc852b3cb
SigmasonicX <sigmasonicx@gmail.com>
2021-11-24 12:02:22 -0800

    Adds log-in button to mobile menu - Profile menu is not optimized for mobile yet

--- libs/client/ui/src/lib/components/nav/mobile-nav/mobile-nav.component.ts
+++ libs/client/ui/src/lib/components/nav/mobile-nav/mobile-nav.component.ts
@@ -1,8 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionQuery } from '@dragonfish/client/repository/session';
+import { PseudonymsQuery } from '@dragonfish/client/repository/pseudonyms';
+import { Roles } from '@dragonfish/shared/models/accounts';
+import { isAllowed } from '@dragonfish/shared/functions';
+import { DragonfishElectronService } from '@dragonfish/client/services';
+import { UserMenuComponent } from '../../auth/user-menu/user-menu.component';
+import { ViewRef, ViewService } from '@ngneat/overview';
+import { NavigationStart, Router } from '@angular/router';
+import { untilDestroyed } from '@ngneat/until-destroy';
+import { combineLatestWith, interval, switchMap } from 'rxjs';
+import { NotificationsRepository } from '@dragonfish/client/repository/notifications';
 
 @Component({
     selector: 'dragonfish-mobile-nav',
     templateUrl: './mobile-nav.component.html',
     styleUrls: ['./mobile-nav.component.scss']
 })
-export class MobileNavComponent {}
+export class MobileNavComponent implements OnInit {
+    userMenu: ViewRef;
+    activeUserMenu = false;
+    constructor(
+        public sessionQuery: SessionQuery,
+        public pseudQuery: PseudonymsQuery,
+        public electron: DragonfishElectronService,
+        private router: Router,
+        private viewService: ViewService,
+        public notifications: NotificationsRepository,
+    ) {}
+
+    ngOnInit(): void {
+        this.router.events.pipe(untilDestroyed(this)).subscribe((event) => {
+            if (event instanceof NavigationStart) {
+                if (this.userMenu) {
+                    this.userMenu.destroy();
+                    this.userMenu = undefined;
+                    this.activeUserMenu = false;
+                }
+            }
+        });
+
+        this.sessionQuery.currAccount$
+            .pipe(combineLatestWith(this.pseudQuery.current$), untilDestroyed(this))
+            .subscribe((values) => {
+                const [account, profile] = values;
+                if (account && profile) {
+                    this.notifications
+                        .getAllUnread()
+                        .pipe(untilDestroyed(this))
+                        .subscribe(() => {
+                            interval(15000)
+                                .pipe(
+                                    switchMap(() => this.notifications.getAllUnread()),
+                                    untilDestroyed(this),
+                                )
+                                .subscribe();
+                        });
+                }
+            });
+    }
+
+    openUserMenu() {
+        if (!this.userMenu) {
+            this.userMenu = this.viewService.createView(UserMenuComponent);
+            document.body.appendChild(this.userMenu.getElement() as any);
+            this.activeUserMenu = true;
+        } else {
+            this.userMenu.destroy();
+            this.userMenu = undefined;
+            this.activeUserMenu = false;
+        }
+    }
+}
