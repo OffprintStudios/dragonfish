0463dc8e4dfe030b885d40960c1f5a8b838d52d4
Alyx Mote <figments@outlook.com>
2021-11-26 09:27:09 -0800

    adding counts for followers

--- libs/api/notifications/src/lib/services/followers.service.ts
+++ libs/api/notifications/src/lib/services/followers.service.ts
@@ -2,12 +2,13 @@ import { Injectable, Logger } from '@nestjs/common';
 import { SubscriptionsStore } from '../db/stores';
 import { Subscription, SubscriptionKind } from '@dragonfish/shared/models/accounts/notifications';
 import { SubscriptionPayload } from '@dragonfish/shared/models/accounts/notifications/payloads';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Injectable()
 export class FollowersService {
     private logger = new Logger(`Followers`);
 
-    constructor(private readonly subscriptions: SubscriptionsStore) {}
+    constructor(private readonly subscriptions: SubscriptionsStore, private readonly events: EventEmitter2) {}
 
     public async checkIfFollowing(pseudId: string, isFollowing: string) {
         return await this.subscriptions.fetchOne(pseudId, isFollowing);
@@ -28,6 +29,18 @@ export class FollowersService {
             kind: SubscriptionKind.FollowingUser,
         };
 
+        const countFollowersPayload = {
+            pseudId: toFollow,
+            numFollowers: await this.subscriptions.countSubscribers(toFollow, SubscriptionKind.FollowingUser),
+        };
+
+        const countFollowingPayload = {
+            pseudId: pseudId,
+            numFollowing: await this.subscriptions.countSubscriptions(pseudId, SubscriptionKind.FollowingUser),
+        };
+
+        this.events.emit('pseudonyms.update-follower-count', countFollowersPayload);
+        this.events.emit('pseudonyms.update-following-count', countFollowingPayload);
         return await this.subscriptions.create(payload);
     }
 
