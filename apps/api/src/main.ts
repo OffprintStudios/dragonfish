1a01a378e9253fda1fa3dc8f56ac2baa9fc4006a
Alyx Mote <figments@outlook.com>
2021-12-07 17:28:04 -0800

    update cors policy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -37,9 +37,10 @@ async function bootstrap() {
     }
     app.use(prerenderNode.set('prerenderToken', process.env.PRERENDER_TOKEN));
     app.enableCors({
-        origin: ['http://localhost:*', 'http://127.0.0.1:*', 'https://offprint.net', /\.offprint\.net$/],
+        origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://offprint.net', /\.offprint\.net$/],
         allowedHeaders: ['Content-Type', 'Authorization'],
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        credentials: true,
     });
     app.setGlobalPrefix('api');
 
