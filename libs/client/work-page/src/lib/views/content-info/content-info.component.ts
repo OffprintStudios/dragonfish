6f5af780c7517e3db3507619634a193d822aac1f
SigmasonicX <sigmasonicx@gmail.com>
2021-12-11 11:19:56 -0800

    Add description dropdown to mobile work page

--- libs/client/work-page/src/lib/views/content-info/content-info.component.ts
+++ libs/client/work-page/src/lib/views/content-info/content-info.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { WorkPageQuery, WorkPageService } from '@dragonfish/client/repository/work-page';
 import { RatingOption } from '@dragonfish/shared/models/reading-history';
 import { SessionQuery } from '@dragonfish/client/repository/session';
 import { AuthService } from '@dragonfish/client/repository/session/services';
+import { isMobile } from '@dragonfish/shared/functions';
 
 @Component({
     selector: 'dragonfish-content-info',
     templateUrl: './content-info.component.html',
     styleUrls: ['./content-info.component.scss'],
 })
-export class ContentInfoComponent {
+export class ContentInfoComponent implements OnInit {
     ratingOption = RatingOption;
+    mobileMode = false;
+    mobileShowDescription = false;
 
     constructor(
         public workQuery: WorkPageQuery,
@@ -18,4 +21,17 @@ export class ContentInfoComponent {
         public session: SessionQuery,
         public auth: AuthService,
     ) {}
+
+    ngOnInit(): void {
+        this.onResize();
+    }
+
+    toggleShowDescription() {
+        this.mobileShowDescription = !this.mobileShowDescription;
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+        this.mobileMode = isMobile();
+    }
 }
