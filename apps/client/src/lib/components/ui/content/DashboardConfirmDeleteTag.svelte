497ff2cb085545cb24de3612c9347601470b2b37
SigmasonicX <sigmasonicx@gmail.com>
2022-04-29 22:34:24 -0700

    Adds Dashboard tags management page - Also create/edit tag panel and delete tag dialog - Modifies popup to support passing in data

---
+++ apps/client/src/lib/components/ui/content/DashboardConfirmDeleteTag.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+    import { CloseLine, DeleteBinLine } from 'svelte-remixicon';
+    import { closePopup, closePopupAndConfirm, getData } from '$lib/components/nav/popup';
+    import Button from '$lib/components/ui/misc/Button.svelte';
+    import { TextField } from '$lib/components/forms';
+    import { createForm } from 'felte';
+    import type { TagsModel } from '$lib/models/content/works';
+    import { tags } from '$lib/services';
+import { pluralize } from '$lib/util';
+
+    const { form, data, errors, isSubmitting } = createForm({
+        onSubmit: async () => {
+            tags.deleteTag(getTag()._id).subscribe(() => {
+                closePopupAndConfirm();
+            })
+        },
+        validate: ((values) => {
+            const errors = {
+                name: '',
+            };
+
+            if (values.name !== getTag().name) {
+                errors.name = 'Incorrect tag name';
+            }
+
+            return errors;
+        }),
+    });
+
+    function getTag(): TagsModel {
+        return getData().tag as TagsModel;
+    }
+</script>
+
+<form use:form>
+    <div class="upload-container bg-zinc-300 dark:bg-zinc-700 pb-4">
+        <div class="flex items-center border-b-2 border-zinc-500 px-4 py-2 w-full">
+            <h3 class="font-medium text-2xl flex-1">Are you sure you want to delete this tag?</h3>
+            <Button on:click={closePopup}>
+                <CloseLine size="20px" class="button-icon no-text" />
+            </Button>
+        </div>
+        <div class="items-center px-4 py-2 w-full">
+            This has {getTag().taggedWorks} tagged work{pluralize(getTag().taggedWorks)}. Type in {getTag().name} to delete it.
+        </div>
+        <TextField
+                name="name"
+                type="text"
+                placeholder="Tag name"
+                errorMessage={$errors.name}
+            />
+        <div class="flex flex-row items-center justify-center w-full">
+            <Button type="submit" loading={$isSubmitting} loadingText="Deleting...">
+                <DeleteBinLine class="button-icon" />
+                <span class="button-text">Yes, delete</span>
+            </Button>
+            <div class="mx-0.5"><!--separator--></div>
+            <Button on:click={closePopup}>
+                <CloseLine class="button-icon" />
+                <span class="button-text">No, cancel</span>
+            </Button>
+        </div>
+    </div>
+</form>
--- apps/client/src/routes/dashboard/tags/index.svelte
+++ apps/client/src/routes/dashboard/tags/index.svelte
@@ -1,4 +1,136 @@
-<div class="empty">
-    <h3>Pardon the dust&mdash;</h3>
-    <p>This page is still under construction. Check back later!</p>
+<script lang="ts">
+    import { TagKind, TagsModel, TagsTree } from "$lib/models/content/works";
+
+    import { tags } from "$lib/services";
+    import { formatTagName, slugify } from "$lib/util";
+
+    import { onMount } from "svelte";
+    import { openPopup, PopupOnConfirm } from '$lib/components/nav/popup';
+    import DashboardTagsForm from "$lib/components/ui/content/DashboardTagsForm.svelte";
+    import { DeleteBinLine } from "svelte-remixicon";
+    import DashboardConfirmDeleteTag from "$lib/components/ui/content/DashboardConfirmDeleteTag.svelte";
+
+    class AlphabeticalTags {
+        tags: TagsTree[] = [];
+        constructor(public letter: string) {
+        }
+    }
+
+    let alphabeticalTags: AlphabeticalTags[] = [];
+
+    onMount(() => {
+        loadTags()
+    })
+
+    function loadTags() {
+        tags.fetchTagsTrees(TagKind.Fandom).subscribe((tagTrees) => {
+            let savedLetter: string;
+            let currentAlphaTags: AlphabeticalTags;
+            alphabeticalTags = [];
+            for (const tag of tagTrees) {
+                // If first letter doesn't match saved letter, save letter and move to new section
+                // Then either way, add to that letter's array
+                if (savedLetter !== tag.name[0].toUpperCase()) {
+                    if (currentAlphaTags) {
+                        alphabeticalTags = [... alphabeticalTags, currentAlphaTags]
+                    }
+                    savedLetter = tag.name[0].toUpperCase();
+                    currentAlphaTags = new AlphabeticalTags(tag.name[0].toUpperCase());
+                }
+                currentAlphaTags.tags.push(tag);
+            }
+            // Add last set
+            if (currentAlphaTags) {
+                alphabeticalTags = [... alphabeticalTags, currentAlphaTags]
+            }
+        });
+    }
+
+    function getChildrenWithWorks(tagsTree: TagsTree): TagsModel[] {
+        const childrenWithWorks: TagsModel[] = [];
+        for (const child of tagsTree.children) {
+            childrenWithWorks.push(child);
+        }
+        return childrenWithWorks;
+    }
+
+    class OnConfirmRefresh implements PopupOnConfirm {
+        onConfirm(): void {
+            loadTags();
+        }
+    }
+</script>
+
+<svelte:head>
+    <title>Tags Management &mdash; Offprint</title>
+</svelte:head>
+
+<div class="w-full h-[calc(100vh-51px)] md:h-screen overflow-y-auto flex flex-col mx-auto p-2">
+    <div class="flex flex-wrap items-center justify-center text-2xl">
+        {#each alphabeticalTags as section, index}
+            {#if index !== 0}
+                &nbsp;
+            {/if}
+            <a href="tags#{section.letter}">
+                {section.letter}
+            </a>
+        {/each}
+    </div>
+    <p>
+    <button on:click={() => openPopup(DashboardTagsForm, new OnConfirmRefresh(), {isCreate: true})}>
+        CREATE TAG
+    </button>
+    <p>
+    {#each alphabeticalTags as section}
+        <h3 class="text-2xl">
+            <a id="{section.letter}">
+                {section.letter}
+            </a>
+        </h3>
+        {#each section.tags as tag}
+            <div>
+                <a
+                    href="/tag/{tag._id}/{slugify(tag.name)}"
+                >
+                    {formatTagName(tag)}
+                </a>
+                ({tag.taggedWorks ? tag.taggedWorks : 0})
+                <button on:click={() => openPopup(DashboardTagsForm, new OnConfirmRefresh(), {isCreate: false, tag: tag})}>
+                    EDIT
+                </button>
+                <button on:click={() => openPopup(DashboardConfirmDeleteTag, new OnConfirmRefresh(), {tag: tag})}>
+                    <DeleteBinLine class="button-icon no-text" />
+                </button>
+                <ul>
+                    {#each getChildrenWithWorks(tag) as child}
+                        <div>
+                            <li>
+                                <a
+                                    href="/tag/{child._id}/{slugify(child.name)}"
+                                >
+                                    {child.name}
+                                </a>
+                                ({child.taggedWorks ? child.taggedWorks : 0})
+                                <button on:click={() => openPopup(DashboardTagsForm, new OnConfirmRefresh(), {isCreate: false, tag: child})}>
+                                    EDIT
+                                </button>
+                                <button on:click={() => openPopup(DashboardConfirmDeleteTag, new OnConfirmRefresh(), {tag: child})}>
+                                    <DeleteBinLine class="button-icon no-text" />
+                                </button>
+                            </li>
+                        </div>
+                    {/each}
+                </ul>
+            </div>
+        {/each}
+        <p>
+    {/each}
 </div>
+
+<style lang="scss">
+    ul {
+        list-style: disc;
+        list-style-position: inside;
+        padding-left: 1rem;
+    }
+</style>
