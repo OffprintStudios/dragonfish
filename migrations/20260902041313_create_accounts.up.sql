8d1afe9a3282a3284d363dc4926affe4bdcfec2b
Alyx Mote <figments@outlook.com>
2026-09-02 18:27:04 -0700

    fixed placement of migrations

--- migrations/20260902041313_create_accounts.up.sql
+++ migrations/20260902041313_create_accounts.up.sql
@@ -1,1 +1,23 @@
 -- Add up migration script here
+CREATE TABLE IF NOT EXISTS accounts (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    roles TEXT[] NOT NULL DEFAULT '{"User"}',
+    terms_agree BOOLEAN NOT NULL DEFAULT FALSE,
+    email_confirmed BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    account_id UUID NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    active_profile CHAR(21) REFERENCES profiles(id) ON DELETE SET NULL,
+    ip_addr TEXT DEFAULT NULL,
+    browser TEXT DEFAULT NULL,
+    device TEXT DEFAULT NULL,
+    os TEXT DEFAULT NULL,
+    expires_on TIMESTAMPTZ NOT NULL DEFAULT (NOW() + interval '30' day),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
