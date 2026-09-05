bbad37be6c1d6694fd572de1d33e9766e496b8a2
Alyx Mote <figments@outlook.com>
2026-09-05 06:18:48 -0700

    Update mod.rs

---
+++ src/database/mod.rs
@@ -0,0 +1,15 @@
+#[cfg(feature = "ssr")]
+pub async fn connect_to_db() -> sqlx::PgPool {
+    let database_url = std::env::var("DATABASE_URL").expect(
+        "Could not find DATABASE_URL! Are you sure the environment is configured correctly?",
+    );
+
+    sqlx::postgres::PgPoolOptions::new()
+        .min_connections(1)
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect(
+            "Could not connect to database! Are you sure the environment is configured correctly?",
+        )
+}
